b0eef632705bad2ffe6579753b834b935c8605ed
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-26 11:29:43 +0700

    A. Watermelon

--- CODEFORCES/Contest/Contest 4/4A.cpp
+++ CODEFORCES/Contest/Contest 4/4A.cpp
@@ -1,26 +1,14 @@
-#include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
+#include <iostream>
 using namespace std;
-using namespace __gnu_pbds;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("file.inp", "r", stdin)
-#define out freopen("file.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+int main() {
+    int w;
+    cin >> w;
 
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
+    if (w % 2 == 0 && w >= 4) 
+        cout << "YES";
+    else 
+        cout << "NO";
 
-signed main() {
-    fastIO;
-    int n;
-    cin >> n;
-    cout << (n & 1 || n < 4 ? "NO" : "YES");
     return 0;
 }
