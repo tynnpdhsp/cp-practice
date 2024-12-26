354f78987cc00006efcc45a2e33abe2c60db6655
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-26 12:04:54 +0700

    Word Processor

--- USACO/987.cpp
+++ USACO/987.cpp
@@ -1,39 +1,31 @@
 #include <iostream>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("word.in", "r", stdin)
-#define out freopen("word.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
+    freopen("word.in", "r", stdin);
+    freopen("word.out", "w", stdout);
 
-signed main() {
-    fastIO; inp; out;
-    int N, K;
-	cin >> N >> K;
+    int n, k, len = 0;
+    cin >> n >> k;
 
-	
-	int len = 0;
-	for (int i = 0; i < N; i++) {
-		string word;
-		cin >> word;
-		len += word.length();
+    for (int i = 1; i <= n; i++) {
+        string s;
+        cin >> s;
 
-		if (len <= K) {
-			if (i != 0) { 
-                cout << ' '; 
-            }
-			cout << word;
+        len += s.length(); 
 
-		} else {
-			cout << "\n" << word;
-			len = word.length();
-		}
-	}
+        if (len <= k) {
+            if (len != s.length())
+                cout << ' ';
+            cout << s;
+        } else {
+            cout << endl << s;
+            len = s.length();
+        }
+    }
 
     return 0;
 }
