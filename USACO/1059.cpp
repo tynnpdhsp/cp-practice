1643888cfb394f1b63deeea6f2c55f487bd88d49
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-26 12:16:21 +0700

    Do You Know Your ABCs?

--- USACO/1059.cpp
+++ USACO/1059.cpp
@@ -2,25 +2,13 @@
 #include <algorithm>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("file.inp", "r", stdin)
-#define out freopen("file.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
-
-signed main() {
-    fastIO; 
+int main() {
     int a[7];
 
-    for (int &x : a) 
-        cin >> x;
+    for (int i = 0; i < 7; i++)
+        cin >> a[i];
 
-    sort(a, a + 7);
-
-    cout << a[0] << ' ' << a[1] << ' ' << a[6] - a[1] - a[0];
+    sort(a,  a + 7);
+    cout << a[0] << ' ' << a[1] << ' ' << a[6] - a[0] - a[1];
     return 0;
 }
