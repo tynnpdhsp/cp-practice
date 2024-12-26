cc59f859ed6b7239045c042c0651f6411369ec70
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-26 12:48:28 +0700

    Promotion Counting

--- USACO/591.cpp
+++ USACO/591.cpp
@@ -1,28 +1,19 @@
 #include <iostream>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("promote.in", "r", stdin)
-#define out freopen("promote.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
+int main() {
+    freopen("promote.in", "r", stdin);
+    freopen("promote.out", "w", stdout);
 
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
+    int dongA, dongB, bacA, bacB, vangA, vangB, bkA, bkB;
+    cin >> dongA >> dongB >> bacA >> bacB >> vangA >> vangB >> bkA >> bkB;
 
-signed main() {
-    fastIO; inp; out;
-    int arr[9];
-    int bronze, silver, gold;
+    int x = bkB - bkA;
+    int y = vangB - vangA + x;
+    int z = bacB - bacA + y;
 
-    for (int i = 1; i <= 8; i++)
-        cin >> arr[i];
-
-    gold = arr[8] - arr[7];
-    silver = arr[6] - arr[5] + gold;
-    bronze = arr[4] - arr[3] + silver;
-
-    cout << bronze << endl << silver << endl << gold;
+    cout << z << endl;
+    cout << y << endl;
+    cout << x;
     return 0;
 }
