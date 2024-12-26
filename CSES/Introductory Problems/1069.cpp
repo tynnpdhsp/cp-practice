a2bdfec4aaf881d7784cffb013877f421b08162b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-26 12:49:01 +0700

    Repetitions

--- CSES/Introductory Problems/1069.cpp
+++ CSES/Introductory Problems/1069.cpp
@@ -1,17 +1,35 @@
 #include <iostream>
 using namespace std;
 
+// int main() {
+//     string s;
+//     int cnt = 1, ans = 1;
+//     cin >> s;
+
+//     for (int i = 1; i < s.size(); i++) {
+//         if (s[i-1] == s[i])
+//             cnt++;
+//         else 
+//             cnt = 1;
+//         ans = max(ans, cnt);
+//     }
+
+//     cout << ans;
+//     return 0;
+// }
+
 int main() {
     string s;
-    int cnt = 1, ans = 1;
     cin >> s;
 
-    for (int i = 1; i < s.size(); i++) {
-        if (s[i-1] == s[i])
-            cnt++;
-        else 
-            cnt = 1;
-        ans = max(ans, cnt);
+    int ans = 0;
+    int i = 0, j = 0, n = s.size();
+
+    while (i < n && j < n) {
+        while (s[i] != s[j]) 
+            i++;
+        ans = max(ans, j - i + 1);
+        j++;
     }
 
     cout << ans;
