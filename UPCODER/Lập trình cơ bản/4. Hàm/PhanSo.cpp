607e309b9aa9f825a9158e6bf62a68d9718d0103
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-28 15:07:25 +0700

    Update PhanSo.cpp

--- UPCODER/Lập trình cơ bản/4. Hàm/PhanSo.cpp
+++ UPCODER/Lập trình cơ bản/4. Hàm/PhanSo.cpp
@@ -4,44 +4,37 @@ using namespace std;
 
 struct PhanSo {
     int tu, mau;
-
+    
     void nhap() {
         cin >> tu >> mau;
-        if (mau < 0) 
-            tu = -tu, 
-            mau = -mau;
-        return;
     }
-
+    
     void xuat() {
-        cout << tu << "/" << mau;
-        return;
+        cout << tu << '/' << mau;
     }
-
+    
     void rutGon() {
         int ucln = __gcd(tu, mau);
         tu /= ucln;
         mau /= ucln;
-        return;
     }
 };
 
-
-
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int size; cin >> size;
-    PhanSo arr[size];
-    for (PhanSo &x : arr) x.nhap();
-
-    PhanSo res = arr[0];
-    for (int i = 1; i < size; i++)
-        if ((double) arr[i].tu/arr[i].mau < (double) res.tu/res.mau)
-            res = arr[i];
-
-    res.rutGon();
-    res.xuat();
+    int n;
+    cin >> n;
+    
+    PhanSo a[n];
+    for (int i = 0; i < n; i++)
+        a[i].nhap();
+    
+    PhanSo kq = a[0];
+    for (int i = 1; i < n; i++) {
+        if (kq.tu * a[i].mau > kq.mau * a[i].tu)
+            kq = a[i];
+    }
+    
+    kq.rutGon();
+    kq.xuat();
     return 0;
 }
