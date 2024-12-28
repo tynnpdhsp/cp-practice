db1a8a516b2818eabf80d08c218b62282032eb52
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-28 15:58:52 +0700

    Update SINHVIEN.cpp

--- UPCODER/Lập trình cơ bản/6. Cấu trúc/SINHVIEN.cpp
+++ UPCODER/Lập trình cơ bản/6. Cấu trúc/SINHVIEN.cpp
@@ -1,59 +1,64 @@
 #include <iostream>
-#define endl '\n'
 using namespace std;
 
 struct SinhVien {
     string HoTen;
     int NamSinh;
     double DTB;
+    
+    void nhap(string ten, int nam, double diem) {
+        HoTen = ten;
+        NamSinh = nam;
+        DTB = diem;
+    }
+    
+    void xuat() {
+        cout << HoTen << ' ' << NamSinh << ' ' << DTB << endl;
+    }
 };
 
-void xuatDiemCao(SinhVien list[], int size) {
-    SinhVien max = list[0];
-    for (int i = 1; i < size; i++) 
-        if (max.DTB < list[i].DTB)
-            max = list[i];
-
-    cout << "Diem cao nhat lop:\n";
-    int cnt = 0;
-    for (int i = 0; i < size; i++) {
-        if (list[i].DTB == max.DTB) {
-            cout << "#" << ++cnt << endl;
-            cout << list[i].HoTen << " ";
-            cout << list[i].NamSinh << " ";
-            cout << list[i].DTB << "\n";
-        }
-    }
-    return;
-}
-
-void xuatDiemThap(SinhVien list[], int size) {
-    SinhVien min = list[0];
-    for (int i = 1; i < size; i++) 
-        if (min.DTB > list[i].DTB)
-            min = list[i];
-
-    cout << "Diem thap nhat lop:\n";
-    int cnt = 0;
-    for (int i = 0; i < size; i++) {
-        if (list[i].DTB == min.DTB) {
-            cout << "#" << ++cnt << endl;
-            cout << list[i].HoTen << " ";
-            cout << list[i].NamSinh << " ";
-            cout << list[i].DTB << "\n";
-        }
-    }
-    return;
-}
-
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    SinhVien list[100];
-    int size = 0;
-    while (cin >> list[size].HoTen >> list[size].NamSinh >> list[size].DTB) size++;
-    xuatDiemCao(list, size);
-    xuatDiemThap(list, size);
+    SinhVien sv[100];
+    int n = 0;
+    
+    string ten;
+    int nam;
+    double diem;
+    
+    while (cin >> ten >> nam >> diem) {
+        sv[n].nhap(ten, nam, diem);
+        n++;
+    }
+    
+    double diemCaoNhat = sv[0].DTB;
+    for (int i = 1; i < n; i++) {
+        if (diemCaoNhat < sv[i].DTB)
+            diemCaoNhat = sv[i].DTB;
+    }
+    
+    double diemThapNhat = sv[0].DTB;
+    for (int i = 1; i < n; i++) {
+        if (diemThapNhat > sv[i].DTB)
+            diemThapNhat = sv[i].DTB;
+    }
+    
+    cout << "Diem cao nhat lop:\n";
+    int cnt = 1;
+    for (int i = 0; i < n; i++) {
+        if (sv[i].DTB == diemCaoNhat) {
+            cout << '#' << cnt++ << endl;
+            sv[i].xuat();
+        }
+    }
+    
+    cout << "Diem thap nhat lop:\n";
+    cnt = 1;
+    for (int i = 0; i < n; i++) {
+        if (sv[i].DTB == diemThapNhat) {
+            cout << '#' << cnt++ << endl;
+            sv[i].xuat();
+        }
+    }
+    
     return 0;
 }
