070ce128bfb55053ec96dbabeca2eacb7c9cd8b7
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-28 14:39:13 +0700

    Update PhanSo1.cpp

--- UPCODER/Lập trình cơ bản/6. Cấu trúc/PhanSo1.cpp
+++ UPCODER/Lập trình cơ bản/6. Cấu trúc/PhanSo1.cpp
@@ -4,24 +4,37 @@ using namespace std;
 
 struct PhanSo {
     int tu, mau;
-} a, b, res;
+};
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+void rutGon(PhanSo &p) {
+    int ucln = __gcd(p.tu, p.mau);
+    p.tu /= ucln;
+    p.mau /= ucln;
+}
 
-    cin >> a.tu >> a.mau;
-    cin >> b.tu >> b.mau;
+PhanSo tinhTong(PhanSo a, PhanSo b) {
+    PhanSo kq;
+    kq.tu = a.tu * b.mau + a.mau * b.tu;
+    kq.mau = a.mau * b.mau;
+    rutGon(kq);
+    return kq;
+}
 
-    if (a.mau == 0 || b.mau == 0) cout << -1;
-    else {
-        res.tu = a.tu*b.mau + b.tu*a.mau;
-        res.mau = a.mau * b.mau;
-        int ucln = __gcd(res.tu, res.mau);
-        res.tu /= ucln;
-        res.mau /= ucln;
-        cout << res.tu << "/" << res.mau;
+void xuatPhanSo(PhanSo p) {
+    if (p.mau != 0) {
+        cout << p.tu << '/' << p.mau;
+        return;
     }
     
+    cout << -1;
+}
+
+int main() {
+    PhanSo a, b;
+    cin >> a.tu >> a.mau;
+    cin >> b.tu >> b.mau;
+    PhanSo c = tinhTong(a, b);
+    xuatPhanSo(c);
     return 0;
 }
+
