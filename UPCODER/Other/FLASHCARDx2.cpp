855e5127c9110d2944fbe1dbea884850fcf1e08d
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-29 18:27:05 +0700

    Update FLASHCARDx2.cpp

--- UPCODER/Other/FLASHCARDx2.cpp
+++ UPCODER/Other/FLASHCARDx2.cpp
@@ -31,3 +31,32 @@ int main() {
     
     return 0;
 }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int n, a[51], res[51];
+
+// int main() {
+//     cin >> n;
+    
+//     for (int i = 1; i <= n; i++)
+//         cin >> a[i];
+        
+//     for (int i = n; i > 0; i--) {
+//         a[i] *= 2;
+//         res[i] += a[i] % 10;
+//         res[i-1] += (a[i] > 9 ? 1 : 0);
+//     }
+    
+//     for (int i = 0; i <= n; i++) {
+//         if (i == 0 && res[i] != 0) 
+//             cout << res[i] << endl;
+
+//         if (i > 0)
+//             cout << res[i] << endl;
+//     }
+    
+//     return 0;
+// }
