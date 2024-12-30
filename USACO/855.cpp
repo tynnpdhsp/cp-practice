c311c0162c874385e87de9a94974e0fac25b01d2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-30 13:09:11 +0700

    Update 855.cpp

--- USACO/855.cpp
+++ USACO/855.cpp
@@ -1,38 +1,25 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("mixmilk.in", "r", stdin)
-#define out freopen("mixmilk.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+int c[3], m[3];
 
-const int MAXN = 3;
-const int MOD = 1e9 + 7;
+int main() {
+    freopen("mixmilk.in", "r", stdin);
+    freopen("mixmilk.out", "w", stdout);
 
-int n = 3;
-int c[MAXN], m[MAXN];
-
-signed main() {
-    fastIO; inp; out;
-
-    for (int i = 0; i < MAXN; i++)
+    for (int i = 0; i < 3; i++)
         cin >> c[i] >> m[i];
-
+    
     for (int i = 0; i < 100; i++) {
-        int cur = i % MAXN;
-        int next = (i + 1) % MAXN;
-
-        // Đảm bảo lượng sữa không quá giới hạn xô next
-        int move = min(m[cur], c[next] - m[next]);
-
-        m[cur] -= move;
-        m[next] += move;
+        int x = i % 3;
+        int y = (i + 1) % 3;
+        
+        int k = min(m[x], c[y] - m[y]);
+        m[y] += k;
+        m[x] -= k;
     }
     
-    for (int &x : m)
-        cout << x << endl;
-
+    cout << m[0] << '\n' << m[1] << '\n' << m[2];
     return 0;
 }
