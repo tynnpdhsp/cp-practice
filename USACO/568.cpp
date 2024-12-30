69e0a09192d3354aaf302afe5a19bb7c76668919
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-30 13:29:05 +0700

    Update 568.cpp

--- USACO/568.cpp
+++ USACO/568.cpp
@@ -1,46 +1,41 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("speeding.in", "r", stdin)
-#define out freopen("speeding.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
-
-const int MAXN = 100 + 5;
-const int MOD = 1e9 + 7;
-
 int n, m, ans;
-int a[MAXN], b[MAXN];
+int x[101], y[101];
 
-signed main() {
-    fastIO; inp; out;
+int main() {
+    freopen("speeding.in", "r", stdin);
+    freopen("speeding.out", "w", stdout);
+
     cin >> n >> m;
-
-    int k = 0;
+    
+    int len = 1;
     while (n--) {
-        int x, y;
-        cin >> x >> y;
-
-        for (int j = k; j < k + x; j++)
-            a[j] = y;
-        k += x;
+        int a, b;
+        cin >> a >> b;
+        
+        for (int i = len; i < len + a; i++)
+            x[i] = b;
+        
+        len += a;
     }
-  
-    k = 0;
+    
+    len = 1;
     while (m--) {
-        int x, y;
-        cin >> x >> y;
-
-        for (int j = k; j < k + x; j++)
-            b[j] = y;
-        k += x;
+        int a, b;
+        cin >> a >> b;
+        
+        for (int i = len; i < len + a; i++)
+            y[i] = b;
+            
+        len += a;
     }
-
-    for (int i = 0; i < 100; i++)
-        ans = max(ans, b[i] - a[i]);
-
+    
+    for (int i = 1; i <= 100; i++)
+        ans = max(ans,  y[i] - x[i]);
+    
     cout << ans;
     return 0;
 }
