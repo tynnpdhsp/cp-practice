fbbf01832fe2c857a528567bf97f68651272d0e4
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-30 22:03:41 +0700

    Update 735.cpp

--- USACO/735.cpp
+++ USACO/735.cpp
@@ -2,27 +2,29 @@
 #include <cmath>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("lostcow.in", "r", stdin)
-#define out freopen("lostcow.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
-
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
-
-signed main() {
-    fastIO; inp; out;
-    int n, d, x, y;
-    cin >> x >> y;
-
-    y -= x;
-    d = abs(y);
+int main() {
+    freopen("lostcow.in", "r", stdin);
+    freopen("lostcow.out", "w", stdout);
     
-    n = ceil(log(d) / log(2));  // Bước n -> duy chuyển 2^n -> duy chuyển tối đa 2^(n+1) - 1 => 2^(n+1) - 1 >= d tìm n >= log2(d)
-    n += (n + (y < 0)) % 2;     // Nếu y < 0 (bên trái) hoặc n lẻ
-
-    cout << 2 * (pow(2, n) - 1) + d;    
+    int x, y;
+    cin >> x >> y;
+    
+    int cur = x, next;
+    int step = 0, ans = 0;
+    
+    while (true) {
+        next = x + pow(-2, step);
+        
+        if ((cur <= y && y <= next) || (cur >= y && y >= next)) {
+            ans += abs(y - cur);
+            break;
+        }
+        
+        ans += abs(cur - next);
+        cur = next;
+        step++;
+    }
+    
+    cout << ans;
     return 0;
 }
