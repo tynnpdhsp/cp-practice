d0985bac3c61e3e38bea4dd3e823d12b63680a66
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2024-12-31 11:47:33 +0700

    Update 615.cpp

--- USACO/615.cpp
+++ USACO/615.cpp
@@ -1,35 +1,17 @@
 #include <iostream>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("pails.in", "r", stdin)
-#define out freopen("pails.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+int main() {
+    freopen("pails.in", "r", stdin);
+    freopen("pails.out", "w", stdout);
 
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
-
-signed main() {
-    fastIO; inp; out;
-    int x, y, m;
+    int x, y, m, ans = 0;
     cin >> x >> y >> m;
 
-    int ans = 0;
-    for (int i = 0; i <= m; i++) {
-        if (x * i > m)
-            break;
-
-        for (int j = 0; j <= m; j++) {
-            int z = x*i + y*j;
-
-            if (z > m)
-                break;
-
-            ans = max(ans, z);
-        }
-    }
+    for (int i = 0; i <= m/x; i++) 
+        for (int j = 0; j <= m/y; j++) 
+            if (x*i + y*j <= m)
+                ans = max(ans, x*i + y*j);
     
     cout << ans;
     return 0;
