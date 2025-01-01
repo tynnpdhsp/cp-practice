23d7c0b4840481689b3603e0498285003c793fc8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-01 11:19:57 +0700

    Daisy Chains

--- USACO/1060.cpp
+++ USACO/1060.cpp
@@ -1,26 +1,62 @@
+// #include <iostream>
+// using namespace std;
+
+// int n, ans;
+// int p[101];
+
+// int main() {
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//         cin >> p[i];
+
+//     for (int i = 0; i < n; i++) {
+//         for (int j = i; j < n; j++) {
+//             int k = j - i + 1;
+//             int s = 0;
+
+//             for (int x = i; x <= j; x++)
+//                 s += p[x];
+
+//             if (s % k != 0)
+//                 continue;
+            
+//             for (int x = i; x <= j; x++) {
+//                 if (p[x] == s/k) {
+//                     ans++;
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     cout << ans;
+//     return 0;
+// }
+
 #include <iostream>
 using namespace std;
 
 int n, ans;
-int p[101];
+int p[101], sum[101];
 
 int main() {
     cin >> n;
 
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         cin >> p[i];
 
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            int k = j - i + 1;
-            int s = 0;
+    for (int i = 1; i <= n; i++)
+        sum[i] = sum[i-1] + p[i];
 
-            for (int x = i; x <= j; x++)
-                s += p[x];
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j <= n; j++) {
+            int k = j - i + 1;
+            int s = sum[j] - sum[i - 1];
 
             if (s % k != 0)
                 continue;
-            
+        
             for (int x = i; x <= j; x++) {
                 if (p[x] == s/k) {
                     ans++;
