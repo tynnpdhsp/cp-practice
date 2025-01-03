4714a1afc8af8f08457e72cfa5e59ec94a836613
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-03 11:27:05 +0700

    Sum of Two Values

--- CSES/Sorting and Searching/1621.cpp
+++ CSES/Sorting and Searching/1621.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
+#include <set>
 #include <algorithm>
 using namespace std;
 
-const int MAXN = 2e5 + 5;
-int n, a[MAXN];
+// const int MAXN = 2e5 + 5;
+// int n, a[MAXN];
+
+// int main() {
+//     cin.tie(0) -> sync_with_stdio(0);
+//     cin >> n;
+
+//     for (int i = 0; i < n; i++)
+//         cin >> a[i];
+
+//     sort(a, a + n);
+
+//     int ans = 1;
+//     for (int i = 1; i < n; i++)
+//         if (a[i-1] != a[i])
+//             ans++;
+
+//     cout << ans;
+//     return 0;
+// }
+
+int n, x;
+set<int> s;
 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
 
-    for (int i = 0; i < n; i++)
-        cin >> a[i];
-
-    sort(a, a + n);
-
-    int ans = 1;
-    for (int i = 1; i < n; i++)
-        if (a[i-1] != a[i])
-            ans++;
-
-    cout << ans;
+    while (n--) {
+        cin >> x;
+        s.insert(x);
+    }
+    
+    cout << s.size();
     return 0;
 }
