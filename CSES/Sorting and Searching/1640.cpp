87bb0dea0ed50b14d42722e875ee33448829df85
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-03 11:59:30 +0700

    Sum of Two Values

--- CSES/Sorting and Searching/1640.cpp
+++ CSES/Sorting and Searching/1640.cpp
@@ -1,10 +1,77 @@
+// #include <iostream>
+// #include <set>
+// using namespace std;
+
+// int n, x, k;
+// bool found;
+// set<pair<int, int>> s;
+
+// int main() {
+//     cin.tie(0) -> sync_with_stdio(0);
+//     cin >> n >> k;
+    
+//     for (int i = 1; i <= n; i++) {
+//         cin >> x;
+//         s.insert({x, i});
+//     }
+    
+//     for (auto p : s) {
+//         int v = k - p.first;
+//         auto it = s.lower_bound({v, 1});
+        
+//         if (it != s.end() && it->first == v && it->second != p.second) {
+//             cout << p.second << ' ' << it->second;
+//             found = true;
+//             break;
+//         }
+//     }
+    
+//     if (!found)
+//         cout << "IMPOSSIBLE";
+//     return 0;
+// }
+    
+    
+    
+// #include <iostream>
+// #include <map>
+// using namespace std;
+
+// int n, x, k;
+// bool found;
+// map<int, int> m;
+
+// int main() {
+//     cin.tie(0) -> sync_with_stdio(0);
+//     cin >> n >> k;
+    
+//     for (int i = 1; i <= n; i++) {
+//         cin >> x;
+        
+//         if (m.count(k - x)) {
+//             cout << i << ' ' << m[k - x];
+//             found = true;
+//             break;
+//         }
+        
+//         m[x] = i;
+//     }
+  
+//     if (!found) 
+//         cout << "IMPOSSIBLE";
+//     return 0; 
+// }
+
+
+
 #include <iostream>
-#include <set>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
 int n, x, k;
 bool found;
-set<pair<int, int>> s;
+vector<pair<int, int>> v;
 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
@@ -12,18 +79,23 @@ int main() {
     
     for (int i = 1; i <= n; i++) {
         cin >> x;
-        s.insert({x, i});
+        v.push_back({x, i});
     }
     
-    for (auto p : s) {
-        int v = k - p.first;
-        auto it = s.lower_bound({v, 1});
+    sort(v.begin(), v.end());
+    int l = 0, r = n - 1;
+    
+    while (l < r) {
+        int m = v[l].first + v[r].first;
         
-        if (it != s.end() && it->first == v && it->second != p.second) {
-            cout << p.second << ' ' << it->second;
+        if (m == k) {
+            cout << v[l].second << ' ' << v[r].second;
             found = true;
             break;
         }
+        
+        if (m > k) r--;
+        else l++;
     }
     
     if (!found)
