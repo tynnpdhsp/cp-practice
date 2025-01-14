9c0d916792a510bf808aa6afacf783caacadac0b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-14 15:08:32 +0700

    Counting Liars

--- USACO/1228.cpp
+++ USACO/1228.cpp
@@ -11,33 +11,30 @@ vector<pair<int, char>> v;
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
+    
     v.resize(n);
+    l.resize(n);
+    g.resize(n);
     
     for (int i = 0; i < n; i++) 
         cin >> v[i].second >> v[i].first;
     
     sort(v.begin(), v.end());
     
-    for (int i = 0; i < n; i++) {
-        l.push_back(0);
-        g.push_back(0);
+    for (int i = g.size()-1; i > 0; i--) {
+        g[i-1] += g[i];
         
-        if (v[i].second == 'L')
-            l[l.size()-1] = 1;
-        else 
-            g[g.size()-1] = 1;
+        if (v[i].second == 'G')
+            g[i-1]++;
     }
     
-    for (int i = g.size()-1; i > 0; i--) 
-        g[i-1] += g[i];
-    g.push_back(0);
-    
-    for (int i = 0; i < g.size()-1; i++)
-        g[i] = g[i+1];
         
-    for (int i = 1; i < l.size(); i++)
+    for (int i = 1; i < l.size(); i++) {
         l[i] += l[i-1];
-    l.insert(l.begin(), 0);
+        
+        if (v[i-1].second == 'L')
+            l[i]++;
+    }
     
     ans = INT_MAX;
     for (int i = 0; i < n; i++)
