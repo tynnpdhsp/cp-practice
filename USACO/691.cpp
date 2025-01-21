9237b715bfa1a40e5f815e1aec9c3b02582f5ec2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-21 11:27:19 +0700

    Hoof, Paper, Scissors

--- USACO/691.cpp
+++ USACO/691.cpp
@@ -1,46 +1,36 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("hps.in", "r", stdin)
-#define out freopen("hps.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
-
 const int MAXN = 1e5 + 5;
-const int MOD = 1e9 + 7;
 
 int n, ans;
-int h[MAXN], p[MAXN], s[MAXN];
+int s[MAXN], p[MAXN], h[MAXN];
 
-signed main() {
-    fastIO; inp; out;
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    freopen("hps.in", "r", stdin);
+    freopen("hps.out", "w", stdout);
     cin >> n;
-
+    
     for (int i = 1; i <= n; i++) {
-        h[i] = h[i - 1];
-        p[i] = p[i - 1];
-        s[i] = s[i - 1];
-
+        s[i] += s[i-1];
+        p[i] += p[i-1];
+        h[i] += h[i-1];
+        
         char c;
         cin >> c;
-
-        if (c == 'H')
-            p[i]++;
-        else if (c == 'P')
-            s[i]++;
-        else    
-            h[i]++;
+        
+        if (c == 'S') h[i]++;
+        if (c == 'P') s[i]++;
+        if (c == 'H') p[i]++;
     }
-
+    
     for (int i = 1; i <= n; i++) {
-        int x = max({h[i], p[i], s[i]});
-        int y = max({h[n] - h[i], p[n] - p[i], s[n] - s[i]});
+        int x = max({s[i], p[i], h[i]});
+        int y = max({s[n] - s[i], p[n] - p[i], h[n] - h[i]});
         ans = max(ans, x + y);
     }
-
+    
     cout << ans;
     return 0;
 }
