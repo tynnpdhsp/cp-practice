1c63b7af1e488ce4431b7bd25cd0837c833469d8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-22 13:42:48 +0700

    Maximum Subarray Sum

---
+++ CSES/Sorting and Searching/1643.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MAXN 200005
+
+int n, ans;
+int a[MAXN], sum[MAXN];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) {
+        cin >> a[i];
+        sum[i] = sum[i-1] + a[i];
+    }
+    
+    int minVal = 0;
+    ans = INT_MIN;
+    
+    for (int i = 1; i <= n; i++) {
+        ans = max(ans, sum[i] - minVal);
+        minVal = min(minVal, sum[i]);
+    }  
+   
+    cout << ans;
+    return 0;
+}
