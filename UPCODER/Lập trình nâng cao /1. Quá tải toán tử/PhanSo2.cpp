7276e309787e97974c915ae7916e2123d7c5f799
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-23 12:18:40 +0700

    Update PhanSo2.cpp

--- UPCODER/Lập trình nâng cao /1. Quá tải toán tử/PhanSo2.cpp
+++ UPCODER/Lập trình nâng cao /1. Quá tải toán tử/PhanSo2.cpp
@@ -1,81 +1,75 @@
-#include <iostream>
-#include <algorithm>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct PhanSo {
-    int tu;
-    int mau;
-
-    friend istream& operator >> (istream& in, PhanSo& p);
-    friend ostream& operator << (ostream& out, PhanSo p);
-    bool operator == (PhanSo p);
-    bool operator != (PhanSo p);
-    PhanSo operator + (PhanSo p);
+    int tu, mau;
+    
+    void rutGon() {
+        int ucln = __gcd(tu, mau);
+        tu /= ucln;
+        mau /= ucln;
+    }
 };
 
-struct MangPhanSo {
+struct Array {
     int size;
-    PhanSo* values;
-
-    friend istream& operator >> (istream& in, MangPhanSo& arr);
-    friend ostream& operator << (ostream& out, MangPhanSo arr);
-    PhanSo operator [] (int index);
+    PhanSo items[100];
+    
+    PhanSo &operator [] (int index) {
+        return items[index];
+    }
 };
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    MangPhanSo arr; cin >> arr;
-    PhanSo res = arr.values[0];
-    for (int i = 1; i < arr.size; i++)
-        res = res + arr.values[i];
-    cout << res;
-    return 0;
-}
-
-istream& operator >> (istream& in, PhanSo& p) {
+istream &operator >> (istream &in, PhanSo &p) {
     in >> p.tu >> p.mau;
+    p.rutGon();
     return in;
 }
 
-ostream& operator << (ostream& out, PhanSo p) {
-    int ucln = __gcd(p.tu, p.mau);
-    p.tu /= ucln;
-    p.mau /= ucln;
+istream &operator >> (istream &in, Array &arr) {
+    arr.size = 0;
+    
+    while (in >> arr[arr.size]) 
+        arr.size++;
+    
+    return in;
+}
+
+ostream &operator << (ostream &out, PhanSo p) {
+    p.rutGon();
     out << p.tu << '/' << p.mau;
     return out;
 }
 
-bool PhanSo::operator == (PhanSo p) {
-    return this->tu*p.mau == this->mau*p.tu;
-}
-
-bool PhanSo::operator != (PhanSo p) {
-    return !(*this == p);
-}
-
-PhanSo PhanSo::operator + (PhanSo p) {
+PhanSo operator + (PhanSo a, PhanSo b) {
     PhanSo res;
-    res.tu = this->tu*p.mau + this->mau*p.tu;
-    res.mau = this->mau * p.mau;
+    res.tu = a.tu * b.mau + b.tu * a.mau;
+    res.mau = a.mau * b.mau;
+    res.rutGon();
     return res;
 }
 
-istream& operator >> (istream& in, MangPhanSo& arr) {
-    arr.values = new PhanSo[100];
-    arr.size = 0;
-    while (in >> arr.values[arr.size]) arr.size++;
-    return in;
-}
-
-ostream& operator << (ostream& out, MangPhanSo arr) {
-    for (int i = 0; i < arr.size; i++)
-        out << arr.values[arr.size] << " ";
+ostream &operator << (ostream &out, Array arr) {
+    PhanSo sum = arr[0];
+    
+    for (int i = 1; i < arr.size; i++)
+        sum = sum + arr[i];
+        
+    out << sum;
     return out;
 }
 
-PhanSo MangPhanSo::operator [] (int index) {
-    if (index >= 0 && index < this->size)
-        return this->values[index];
+bool operator == (PhanSo a, PhanSo b) {
+    return a.tu * b.mau == b.tu * a.mau;
+}
+
+bool operator != (PhanSo a, PhanSo b) {
+    return !(a == b);
+}
+
+int main() {
+    Array arr;
+    cin >> arr;
+    cout << arr;
+    return 0;
 }
