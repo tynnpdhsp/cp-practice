ea86532526858705e9ca0632e9d627d0f1198988
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-23 12:14:21 +0700

    Update GK_LTNC_CAU2.cpp

--- UPCODER/Other/GK_LTNC_CAU2.cpp
+++ UPCODER/Other/GK_LTNC_CAU2.cpp
@@ -1,55 +1,60 @@
-#include <iostream>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
 
-const double EPSILON = 1e-9; 
+const double E = 1e-6;
 
 struct Diem {
     double x, y;
+};
 
-    friend istream &operator >> (istream &in, Diem &diem) {
-        in >> diem.x >> diem.y;
-        return in;
-    }
-
-    friend ostream &operator << (ostream &out, Diem diem) {
-        out << "(" << diem.x << "," << diem.y << ")";
-        return out;
-    }
-
-    double doDai(Diem other) {
-        return sqrt(pow(other.x - x, 2) + pow(other.y - y, 2));
-    }
-}; 
-
-bool kiemTraTamGiac(double AB, double BC, double CA) {
-    return AB + BC > CA && AB + CA > BC && BC + CA > AB;
+istream &operator >> (istream &in, Diem &d) {
+    in >> d.x >> d.y;
+    return in;
 }
 
-bool tamGiacVuong(double AB, double BC, double CA) {
-    return abs(AB*AB + BC*BC - CA*CA) < EPSILON || abs(BC*BC + CA*CA - AB*AB) < EPSILON || abs(AB*AB + CA*CA - BC*BC) < EPSILON;
+ostream &operator << (ostream &out, Diem d) {
+    out << "(" << d.x << "," << d.y << ")";
 }
 
-char gocVuong(double AB, double BC, double CA) {
-    if (abs(AB*AB + BC*BC - CA*CA) < EPSILON) return 'B';
-    else if (abs(BC*BC + CA*CA - AB*AB) < EPSILON) return 'C';
-    else if (abs(AB*AB + CA*CA - BC*BC) < EPSILON) return 'A';
+double operator + (Diem u, Diem v) {
+    return sqrt(pow(u.x - v.x, 2) + pow(u.y - v.y,2));
+}
+
+bool kiemTraTamGiac(double AB, double AC, double BC) {
+    return AB + BC > AC && AB + AC > BC && AC + BC > AB;
+}
+
+bool tamGiacVuong(double AB, double AC, double BC) {
+    return (abs(AB*AB + AC*AC - BC*BC) < E || abs(AB*AB + BC*BC - AC*AC) < E || abs(AC*AC + BC*BC - AB*AB) < E);
+}
+
+char timGocVuong(double AB, double AC, double BC) {
+    if (abs(AB*AB + AC*AC - BC*BC) < E) return 'A';
+    if (abs(AB*AB + BC*BC - AC*AC) < E) return 'B';
+    if (abs(AC*AC + BC*BC - AB*AB) < E) return 'C';
 }
 
 int main() {
-    Diem A, B, C;
-    double AB, BC, CA;
-    cin >> A >> B >> C;
-
-    AB = A.doDai(B);
-    BC = B.doDai(C);
-    CA = C.doDai(A);
-
-    cout << A << "," << B << "," << C << endl;
-    if (kiemTraTamGiac(AB, BC, CA)) { 
+    Diem a, b, c;
+    cin >> a >> b >> c;
+    cout << a << ',' << b << ',' << c << endl;
+    
+    double AB = a + b;
+    double AC = a + c;
+    double BC = b + c;
+    
+    if (kiemTraTamGiac(AB, AC, BC)) {
         cout << "CO\n";
-        if (tamGiacVuong(AB, BC, CA)) cout << gocVuong(AB, BC, CA);
-    } else cout << "KHONG";
-
+        
+        if (tamGiacVuong(AB, AC, BC)) {
+            cout << timGocVuong(AB, AC, BC);
+        }
+        
+    } else {
+        cout << "KHONG\n";
+    }
+    
     return 0;
 }
+
+
