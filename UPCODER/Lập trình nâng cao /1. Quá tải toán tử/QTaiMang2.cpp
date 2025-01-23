a7f154ad24dec9ec963fdd8bae9b83ba98a4a398
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-23 12:17:56 +0700

    Update QTaiMang2.cpp

--- UPCODER/Lập trình nâng cao /1. Quá tải toán tử/QTaiMang2.cpp
+++ UPCODER/Lập trình nâng cao /1. Quá tải toán tử/QTaiMang2.cpp
@@ -1,69 +1,77 @@
 #include <iostream>
 using namespace std;
 
-struct MangMotChieu {
+struct Array {
     int size;
-    int *values;
-
-    MangMotChieu& operator = (MangMotChieu& other) {
-        this->size = other.size;
+    int items[100];
+    
+    Array operator = (Array other) {
+        size = other.size;
         for (int i = 0; i < other.size; i++)
-            this->values[i] = other[i];
-        return *this;
+            items[i] = other.items[i];
+        return other;
     }
-
-    MangMotChieu operator + (MangMotChieu arr) {
-        MangMotChieu res;
-        res.size = max(this->size, arr.size);
-        res.values = new int[res.size];
-        for (int i = 0; i < res.size; i++)
-            res.values[i] = this->values[i] + arr.values[i];
-        return res;
-    }
-
-    bool operator == (MangMotChieu other) {
-        if (this->size != other.size) return false;
-        for (int i = 0; i < other.size; i++)
-            if (this->values[i] != other.values[i])
-                return false;
-        return true;
-    }
-
-    bool operator != (MangMotChieu other) {
-        if (this->size == other.size) return false;
-        for (int i = 0; i < other.size; i++)
-            if (this->values[i] == other.values[i])
-                return false;
-        return true;
-    }
-
-    int operator [] (int index) {
-        if (index > 0 && index < this->size) 
-            return this->values[index];
+    
+    int &operator [] (int index) {
+        return items[index];
     }
 };
 
-ostream& operator << (ostream& out, MangMotChieu arr) {
-    for (int i = 0; i < arr.size; i++)
-        out << arr.values[i] << " ";
-    return out;
-}
-
-istream& operator >> (istream& in, MangMotChieu& arr) {
+istream& operator >> (istream &in, Array &arr) {
     in >> arr.size;
-        arr.values = new int[arr.size];
-        for (int i = 0; i < arr.size; i++)
-        in >> arr.values[i];
+    for (int i = 0; i < arr.size; i++)
+        in >> arr[i];
     return in;
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+ostream& operator << (ostream &out, Array arr) {
+    for (int i = 0; i < arr.size; i++)
+        out << arr[i] << ' ';
+    return out;
+}
 
-    MangMotChieu arr1, arr2;
-    cin >> arr1 >> arr2;
-    MangMotChieu res = arr1 + arr2;
-    cout << res;
+Array operator + (Array a, Array b) {
+    Array res;
+    res.size = max(a.size, b.size);
+    
+    int i = 0, j = 0;
+    
+    while (i < a.size && j < b.size) {
+        res[i] = a[i] + b[j];
+        i++, j++;
+    }
+    
+    while (i < a.size) {
+        res[i] = a[i];
+        i++;
+    }
+    
+    while (j < b.size) {
+        res[j] = b[j];
+        j++;
+    }
+    
+    return res;
+}
+
+bool operator == (Array a, Array b) {
+    if (a.size != b.size)
+        return false;
+        
+    for (int i = 0; i < a.size; i++) 
+        if (a[i] != b[i])
+            return false;
+            
+    return true;
+}
+
+bool operator != (Array a, Array b) {
+    return !(a == b);
+}
+
+int main() {
+    Array a, b;
+    cin >> a >> b;
+    cout << (a + b);
     return 0;
 }
