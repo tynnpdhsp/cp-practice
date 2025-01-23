2200cd03516cdf5b9e210e46a263c52a584978a2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-23 23:03:08 +0700

    Update KhuonHinhSS.cpp

--- UPCODER/Lập trình nâng cao /2. Khuôn hình/KhuonHinhSS.cpp
+++ UPCODER/Lập trình nâng cao /2. Khuôn hình/KhuonHinhSS.cpp
@@ -1,42 +1,42 @@
 #include <iostream>
-#include <algorithm>
 using namespace std;
 
 struct PhanSo {
     int tu, mau;
-
-    friend istream& operator >> (istream &in, PhanSo &p) {
-        in >> p.tu >> p.mau;
-        return in;
-    }
-
-    friend ostream& operator << (ostream &out, PhanSo p) {
-        out << p.tu << "/" << p.mau;
-        return out;
-    }
-
-    bool operator == (PhanSo other) {
-        return tu*other.mau == mau*other.tu;
-    }
 };
 
-template <typename T>
+istream &operator >> (istream &in, PhanSo &p) {
+    in >> p.tu >> p.mau;
+    return in;
+}
+
+ostream &operator << (ostream &out, PhanSo &p) {
+    out << p.tu << '/' << p.mau;
+    return out;
+}
+
+bool operator == (PhanSo a, PhanSo b) {
+    return a.tu * b.mau == a.mau * b.tu;
+}
+
+template<typename T>
 void soSanh() {
     T a, b;
     cin >> a >> b;
-    cout << (a == b ? "true" : "false");
-    return;
+    
+    if (a == b) 
+        cout << "true";
+    else 
+        cout << "false";
 }
 
 int main() {
-    char kt; 
-    cin >> kt;
-
-    switch (kt) {
-        case 'a': soSanh<int>(); break;
-        case 'b': soSanh<double>(); break;
-        case 'c': soSanh<PhanSo>(); break;
-    }
-
+    char c;
+    cin >> c;
+    
+    if (c == 'a') soSanh<int>();
+    if (c == 'b') soSanh<float>();
+    if (c == 'c') soSanh<PhanSo>();
+    
     return 0;
 }
