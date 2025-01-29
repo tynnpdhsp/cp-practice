7135082f90954f883f8fac8e7bdb9dea965debca
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-29 17:34:36 +0700

    Đếm cặp

--- ONLINE JUDGE/LQDOJ/cpair.cpp
+++ ONLINE JUDGE/LQDOJ/cpair.cpp
@@ -1,27 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-const int MAXN = 1e5 + 5;
+#define int int64_t
+const int MAXN = 1e6 + 5;
 
 int n, k, a[MAXN];
-map<int, int> m;
+unordered_map<int, int> m;
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n >> k;
-    
-    for (int i = 1; i <= n; i++)
+
+    for (int i = 1; i <= n; i++) 
         cin >> a[i];
-    
+
     int ans = 0;
+
     for (int i = 1; i <= n; i++) {
-        int x = k - a[i] * a[i];
-          
-        if (m.find(x) != m.end())
-            ans += m[x];
-        
         m[a[i]]++;
+        int x = k - a[i] * a[i]; 
+        ans += m[x];
     }
 
     cout << ans;
