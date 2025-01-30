e838d3132056668f3819f9a205c0738515a6b24a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-30 17:32:55 +0700

    Update Pointer_MissingPrime.cpp

--- UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_MissingPrime.cpp
+++ UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_MissingPrime.cpp
@@ -1,29 +1,26 @@
 #include <iostream>
 using namespace std;
 
+bool snt(int n) {
+    for (int i = 2; i*i <= n; i++)
+        if (n % i == 0)
+            return false;
+    return n > 1;
+}
+
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    bool *check = new bool[10000];
-    for (int i = 0; i <= 10000; i++)
-        check[i] = true;
-    check[0] = false;
-    check[1] = false;
-
-    for (int i = 2; i*i <= 10000; i++) {
-        if (check[i] == true) {
-            for (int j = i*i; j <= 10000; j += i)
-                check[j] = false;
-        } 
-    }
-
-    int a, b; 
-    cin >> a >> b;
-
-    for (int i = a; i <= b; i++) 
-        if (check[i]) cout << i << " ";
-
-    delete[] check;
+    int l, r, n = 0;
+    int* a = new int[100];
+    
+    cin >> l >> r;
+    
+    for (int i = l; i <= r; i++) 
+        if (snt(i)) 
+            a[n++] = i;
+        
+    for (int i = 0; i < n; i++)
+        cout << a[i] << ' ';
+    
+    delete[] a;
     return 0;
 }
