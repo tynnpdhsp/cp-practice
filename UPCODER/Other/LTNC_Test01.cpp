7d9957b1ba8a1c92de9f75d50bbfedb06d992240
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-30 17:48:19 +0700

    Update LTNC_Test01.cpp

--- UPCODER/Other/LTNC_Test01.cpp
+++ UPCODER/Other/LTNC_Test01.cpp
@@ -1,27 +1,32 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
 int main() {
-    int n, m, tmp, cnt = 0;
+    int n, m;
     cin >> n >> m;
-
-    bool *arr = new bool[1000];
-    memset(arr, false, sizeof(arr));
-
-    for (int i = 0; i < n; i++) {
-        cin >> tmp;
-        arr[tmp] = true;
-    }
     
-    for (int i = 0; i < m; i++) {
-        cin >> tmp;
-        if (!arr[tmp]) {
-            cnt += 1;
-            arr[tmp] = true;
-        }
-    }
-
-    cout << cnt;
+    int* a = new int[n];
+    int* b = new int[m];
+    int* check = new int[100];
+    
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+        
+    for (int i = 0; i < m; i++)
+        check[b[i]] += 1;
+    
+    for (int i = 0; i < n; i++)
+        check[a[i]] = 0;
+        
+    int ans = 0;
+    for (int i = 0; i < 100; i++)
+        ans += check[i] > 0;
+        
+    cout << ans;
+    
+    delete[] a, b, check;
     return 0;
 }
