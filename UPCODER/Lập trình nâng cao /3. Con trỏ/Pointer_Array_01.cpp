d3739778c2dd7ea94ea82d7158e47830abbf04de
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-30 17:37:59 +0700

    Update Pointer_Array_01.cpp

--- UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_Array_01.cpp
+++ UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_Array_01.cpp
@@ -1,35 +1,31 @@
 #include <iostream>
 using namespace std;
 
-bool check(int* arr, int n) {
-    for (int i = 0; i <= n/2; i++) 
-        if (*(arr + i) != *(arr + n - i - 1))
+bool ktDoiXung(int a[], int n) {
+    for (int i = 0; i < n/2; i++)
+        if (a[i] != a[n-i-1])
             return false;
     return true;
 }
 
-void print(int* arr, int n) {
-    if (check(arr, n)) cout << "mang doi xung";
-    else {
-        cout << "mang khong doi xung" << endl;
-        for (int i = 0; i < n/2; i++) 
-            if (*(arr + i) != *(arr + n - i - 1))
-                cout << *(arr + i) << " " << *(arr + n - i - 1) << endl;
-    }
-    return;
-}
-
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
-    int* arr = new int[n];
-
-    for (int i = 0; i < n; i++) 
-        cin >> arr[i];
-
-    print(arr, n);
-    delete[] arr;
+    int n;
+    cin >> n;
+    
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+        
+    if (ktDoiXung(a, n)) {
+        cout << "mang doi xung";
+        
+    } else {
+        cout << "mang khong doi xung\n";
+        for (int i = 0; i < n/2; i++)
+            if (a[i] != a[n-i-1]) 
+                cout << a[i] << ' ' << a[n-i-1] << endl;
+    }
+    
+    delete[] a;
     return 0;
 }
