f13f3b4276caae47f8ae2c7633b4ff0675c1f42e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-01-30 17:42:42 +0700

    Update Pointer_Mang_2.cpp

--- UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_Mang_2.cpp
+++ UPCODER/Lập trình nâng cao /3. Con trỏ/Pointer_Mang_2.cpp
@@ -2,27 +2,25 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
-    int** arr = new int*[n];
-    for (int i = 0; i < n; i++) 
-        arr[i] = new int[n];
-
-    for (int i = 0; i < n; i++) 
-        for (int j = 0; j < n; j++)
-            arr[i][j] = i == j ? 1 : 0;
-
+    int n;
+    cin >> n;
+    
+    int** a = new int*[n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
+        
+    for (int i = 0; i < n; i++)
+        a[i][i] = 1;
+        
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) 
-            cout << arr[i][j] << " ";
+        for (int j = 0; j < n; j++)
+            cout << a[i][j] << ' ';
         cout << endl;
     }
-
-    for (int i = 0; i < n; i++)
-        delete[] arr[i];
-    delete[] arr;
     
+    for (int i = 0; i < n; i++)
+        delete[] a[i];
+    
+    delete[] a;
     return 0;
 }
