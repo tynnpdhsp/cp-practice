0a545523b48f632bd158a6336ed6af6d96b2c21b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-01 16:32:37 +0700

    Phép toán số học

---
+++ ONLINE JUDGE/LQDOJ/basic05.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int64_t a, b;
+    cin >> a >> b;
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+    cout << a / b;
+    return 0;
+}
