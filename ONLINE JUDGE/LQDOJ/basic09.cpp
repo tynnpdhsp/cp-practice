350b5e3e1dcfc92b84574a904a697eb16c9293bc
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-01 16:33:49 +0700

    Ngày tháng năm

---
+++ ONLINE JUDGE/LQDOJ/basic09.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int ngay, thang, nam;
+    cin >> ngay >> thang >> nam;
+    cout << ngay << '/' << thang << '/' << nam;
+    return 0;
+}
