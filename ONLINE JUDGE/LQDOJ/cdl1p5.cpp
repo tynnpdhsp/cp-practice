ce9ada0c5a69c9996d1c3b2b85a897f5cab3224a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 15:13:55 +0700

    Cánh diều - TONG3SO - Tổng ba số

---
+++ ONLINE JUDGE/LQDOJ/cdl1p5.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int64_t a, b, c;
+    cin >> a >> b >> c;
+    cout << "Tong ba so: " << a + b + c << endl;
+    cout << "Tong binh phuong ba so: " << a*a + b*b + c*c;
+    return 0;
+}   
