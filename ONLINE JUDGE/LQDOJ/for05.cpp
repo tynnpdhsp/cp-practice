9431e80704c9b06b000239711161bd739af468e3
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 14:39:36 +0700

    Số hoàn hảo

--- ONLINE JUDGE/LQDOJ/for05.cpp
+++ ONLINE JUDGE/LQDOJ/for05.cpp
@@ -2,21 +2,34 @@
 using namespace std;
 
 int main() {
-    int64_t n, sum = 0;
+    cin.tie(0) -> sync_with_stdio(0);
+    int64_t n, tmp;
+    int64_t sum = 1;
     cin >> n;
 
-    for (int i = 1; i*i <= n; i++) {
-        if (n % i == 0) {
-            sum += i;
-            if (i != n/i)
-                sum += n/i;
+    tmp = n;
+    for (int64_t p = 2; p*p <= n; p++) {
+        if (n % p == 0) {
+            int64_t poSum = 1;
+            int64_t po = 1;
+
+            while (n % p == 0) {
+                po *= p;
+                poSum += po;
+                n /= p; 
+            }
+
+            sum *= poSum;
         }
     }
 
-    if (sum == n*2)
+    if (n > 1) 
+        sum *= (1 + n);
+
+    if (sum == tmp * 2)
         cout << "YES";
     else 
         cout << "NO";
-    
+
     return 0;
 }
