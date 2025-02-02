aec89881772d96a45431f3928565fa2b06f19e41
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 14:03:08 +0700

    Sắp xếp không tăng

---
+++ ONLINE JUDGE/LQDOJ/sortcb01.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for (int &x : a) cin >> x;  
+    sort(a, a + n, greater<int>());
+    for (int x : a) cout << x << ' ';
+    return 0;
+}
