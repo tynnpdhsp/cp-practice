68921476a4e547373e9c3571976bb255e62e3510
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 13:54:19 +0700

    Hình tròn

---
+++ ONLINE JUDGE/LQDOJ/basic12.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double r, pi = 3.14;
+    cin >> r;
+    cout << fixed << setprecision(1);
+    cout << 2*pi*r << endl;
+    cout << pi*r*r << endl;
+    return 0;
+}
