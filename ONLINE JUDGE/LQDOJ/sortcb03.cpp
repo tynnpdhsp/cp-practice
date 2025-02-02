9154918f38bc0c56b5505b2dab9db57da1c9fb4c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 14:41:41 +0700

    Số lớn thứ k

---
+++ ONLINE JUDGE/LQDOJ/sortcb03.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    int a[n];
+    for (int &x : a) cin >> x;
+    sort(a, a + n, greater<int>());
+    cout << a[k-1];
+    return 0;
+}
