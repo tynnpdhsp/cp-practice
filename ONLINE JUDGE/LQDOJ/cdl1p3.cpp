5f97ba5ff8b0436992921762a6926c161960111a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-02 14:58:30 +0700

    Cánh diều - KILOPOUND - Đổi kilo ra pound

---
+++ ONLINE JUDGE/LQDOJ/cdl1p3.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    const double pound = 2.205;
+    double kg;
+    cin >> kg;
+    cout << fixed << setprecision(2) << kg * pound;
+    return 0;
+}
