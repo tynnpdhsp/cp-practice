9061ac1d77b1a8cbb134e65a80c77f4de84f736a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-04 13:37:17 +0700

    Cánh diều - BASODUONG - Kiểm tra ba số có dương cả không

---
+++ ONLINE JUDGE/LQDOJ/cdl2p3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a > 0 && b > 0 && c > 0)
+        cout << "Ca ba so deu duong";
+    else 
+        cout << "It nhat mot so am";
+
+    return 0;
+}
