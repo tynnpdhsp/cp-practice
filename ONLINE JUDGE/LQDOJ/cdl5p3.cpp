7cb00d33e42dd7bfa097c39e4d3f1cff91c3642b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-04 13:19:07 +0700

    Cánh diều - FULLNAME - Nối họ, đệm, tên

---
+++ ONLINE JUDGE/LQDOJ/cdl5p3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    string a, b, c;
+    getline(cin, a);
+    getline(cin, b);
+    getline(cin, c);
+    cout << a << ' ' << b << ' ' << c;
+    return 0;
+}
