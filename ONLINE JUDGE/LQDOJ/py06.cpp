e0d0e89b445a4a2bc7b938c4e97eb49b0b730737
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-04 13:27:56 +0700

    Diện tích, chu vi

---
+++ ONLINE JUDGE/LQDOJ/py06.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double dai = 7.8;
+    double rong = 6.4;
+    cout << "DT = " << dai * rong << endl;
+    cout << "CV = " << (dai + rong) * 2;
+    return 0;
+}
