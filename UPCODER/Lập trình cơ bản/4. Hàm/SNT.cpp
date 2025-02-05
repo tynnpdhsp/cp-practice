924812d4b2c45079e5ba45d1055060fa77fb77b7
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-05 12:36:23 +0700

    Update SNT.cpp

--- UPCODER/Lập trình cơ bản/4. Hàm/SNT.cpp
+++ UPCODER/Lập trình cơ bản/4. Hàm/SNT.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool soNguyenTo(int n) {
+bool prime(int n) {
     for (int i = 2; i*i <= n; i++)
-        if (n % i == 0) return false;
+        if (n % i == 0)
+            return false;
     return n > 1;
 }
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
-    if (soNguyenTo(n)) cout << "true";
-    else cout << "false";
+    int n;
+    cin >> n;
+    cout << (prime(n) ? "true" : "false");
+    return 0;
 }
