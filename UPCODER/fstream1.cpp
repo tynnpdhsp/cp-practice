0296ed0a3c244d4f35f5393e6a5514fc83a0af9a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-05 23:14:03 +0700

    Nhập file & xuất chuẩn

---
+++ UPCODER/fstream1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    freopen("input.txt", "r", stdin);
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}
