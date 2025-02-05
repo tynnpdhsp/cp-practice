4095a523ab7c2d19a836ada66da596cb8adb4f32
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-05 17:18:16 +0700

    Thực hiện phép toán

---
+++ UPCODER/PHEPTOAN.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b; 
+    char c; 
+    cin >> a >> b >> c;
+
+    cout << a << c << b << "=";
+    
+    switch (c) {
+        case '+': cout << a + b; break;
+        case '-': cout << a - b; break;
+        case '*': cout << a * b; break;
+        case '%': cout << a % b; break;
+    }
+    
+    return 0;
+}
