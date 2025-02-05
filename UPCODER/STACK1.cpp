a5ad098a61e9b3e74551e4c74666142e5ebb156e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-06 00:24:15 +0700

    stack cơ bản

---
+++ UPCODER/STACK1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string str;
+    stack<char> s;
+    
+    cin >> str;
+    
+    for (char c : str) {
+        if (c != '*') {
+            s.push(c);
+            continue;
+        }
+        
+        if (!s.empty()) {
+            cout << s.top();
+            s.pop();
+        }
+    }
+    
+    return 0;
+}
