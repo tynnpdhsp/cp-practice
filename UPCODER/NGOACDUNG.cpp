093b1d2bd0031febde62c313764bd727c50543cf
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-06 00:22:46 +0700

    Xâu ngoặc đúng

---
+++ UPCODER/NGOACDUNG.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(string str) {
+    stack<char> s;
+    
+    for (char c : str) {
+        if (c == '(' || c == '{' || c == '[' || s.empty()) {
+            s.push(c);
+            continue;
+        }
+        
+        if (
+            s.top() == '(' && c == ')' ||
+            s.top() == '[' && c == ']' ||
+            s.top() == '{' && c == '}'
+        ) s.pop();
+    }
+        
+    return s.empty();
+}
+
+int main() {
+    string str;
+    getline(cin, str);
+    cout << (check(str) ? "yes" : "no");
+    return 0;
+}
