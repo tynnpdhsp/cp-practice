22ef842b50b6dbc74a2e11a48e4b56a6d3ea1261
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-06 00:43:24 +0700

    UCLN

---
+++ UPCODER/UCLN.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    while (b != 0) {
+        int r = a % b;
+        a = b;
+        b = r;
+    }
+
+    cout << a;
+    return 0;
+}
