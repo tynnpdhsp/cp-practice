340e2f93af644c456cfe343fbcf24630c9e59d3c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-05 12:34:38 +0700

    Update SoCPhuong.cpp

--- UPCODER/Lập trình cơ bản/2. Lệnh rẽ nhánh/SoCPhuong.cpp
+++ UPCODER/Lập trình cơ bản/2. Lệnh rẽ nhánh/SoCPhuong.cpp
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
+    int n; 
+    cin >> n;
     if ((int) sqrt(n) == sqrt(n)) cout << "yes";
     else cout << "no";
-
     return 0;
 }
