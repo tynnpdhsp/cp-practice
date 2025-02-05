56fe9f9db3208cee0a0e5d34d457ad15cbf2e82b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-05 12:41:39 +0700

    Update SoHoanHao.cpp

--- UPCODER/Lập trình cơ bản/4. Hàm/SoHoanHao.cpp
+++ UPCODER/Lập trình cơ bản/4. Hàm/SoHoanHao.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-bool hoanHao(int n) {
-    int res = 0;
+bool perfect(int n) {
+    int sum = 0;
+    
     for (int i = 1; i < n; i++)
-        if (n % i == 0) res += i;
-    return res == n;
+        if (n % i == 0)
+            sum += i;
+    
+    return (sum == n);
 }
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n; cin >> n;
-    cout << (hoanHao(n) ? "Yes" : "No");
+main() {
+    int n;
+    cin >> n;
+    cout << (perfect(n) ? "Yes" : "No");
     return 0;
 }
