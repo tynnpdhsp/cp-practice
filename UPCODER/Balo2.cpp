f9ee67411fa28b1ef466f955119e4afb3572d03c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-06 21:41:59 +0700

    Balo 2

---
+++ UPCODER/Balo2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, w, m;
+const int MAXN = 1001;
+int a[MAXN], c[MAXN];
+int dp[MAXN][MAXN];
+list<int> lst;
+
+int main() {
+    cin >> n >> w;
+    for (int i = 1; i <= n; i++)
+        cin >> a[i] >> c[i];
+        
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j <= w; j++) {
+            dp[i][j] = dp[i-1][j];
+            if (a[i] <= j)
+                dp[i][j] = max(dp[i][j], dp[i-1][j-a[i]] + c[i]);
+        }
+    }
+    
+    m = w;
+    for (int i = n; i > 0; i--) 
+        if (dp[i][m] != dp[i-1][m]) 
+            lst.push_front(1),
+            m -= a[i];
+        else 
+            lst.push_front(0);
+    
+    cout << dp[n][w] << endl;
+    for (auto x : lst) cout << x << " ";
+    return 0;
+}
