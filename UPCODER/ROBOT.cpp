774afb6e3e2849f95bd56d38a8510e56f009d00b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-07 16:12:09 +0700

    Robot tìm đường

---
+++ UPCODER/ROBOT.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int a[100][100];
+string tmp, MAX;
+
+int dx[] = {0, 1};
+int dy[] = {1, 0};
+
+int trans(string num) {
+    int res = 0;
+    int len = num.size();
+    for (int i = 0; i < len; i++)
+        if (num[i] == '1') 
+            res += pow(2, len-1-i);
+    return res;
+}
+
+void Try(int x, int y) {
+    if (x == n && y == n) {
+        if (tmp > MAX) MAX = tmp;
+        return;
+    } 
+    
+    for (int i = 0; i < 2; i++) {
+        int xx = x + dx[i];
+        int yy = y + dy[i];
+            
+        if (xx >= 1 && x <= n && yy >= 1 && yy <= n) {
+            tmp += a[xx][yy] + '0';
+            Try(xx, yy);
+            tmp.pop_back();
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++) 
+            cin >> a[i][j];
+    
+    tmp += a[1][1] + '0';
+    Try(1, 1);
+    cout << trans(MAX);
+    return 0;
+}
