8462627998a5ff7c12d6fa6613a836fd8b9c6799
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 13:19:46 +0700

    Tribonacci

---
+++ UPCODER/OLP16.Lan5.A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAXN 105
+int64_t n, a[MAXN];
+
+int main() {
+    a[2] = 1;
+    
+    for (int i = 3; i <= 100; i++)
+        a[i] = a[i-1] + a[i-2] + a[i-3];
+    
+    while (cin >> n) {
+        int k = 0;
+        while (v[k] < n) k++;
+        cout << v[k] << '\n';
+    }
+    
+    return 0;
+}
