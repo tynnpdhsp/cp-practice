d333fdd0b43f66b5042586631c8f3f7a812acebe
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 22:01:25 +0700

    Tách tên

---
+++ UPCODER/[LTNC]Final03.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    string s; 
+    vector<string> st;
+    while (cin >> s) 
+        t.push_back(s);
+
+    if (st.size() == 2) 
+        cout << st[0] << endl << st[1]; 
+    else {
+        cout << st[0] << endl;
+        for (int i = 1; i < st.size()-1; i++)
+            cout << st[i] << " ";
+        cout << endl << st.back();
+    }
+    
+    return 0;
+}
