e14fa3573d539970563064d5855dd1da98015242
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 21:26:12 +0700

    BT01

---
+++ UPCODER/001.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+ 
+bool checkNT(int n) {
+    for (int i = 2; i*i <= n; i++)
+        if (n % i == 0) 
+            return false;
+    return n > 1;
+}
+
+int main() {
+    int n; 
+    cin >> n; 
+    int a[n];
+    for (int &x : a) cin >> x;
+    bool check = false;
+
+    for (int x : a) {
+        if (checkNT(x)) {
+            cout << x << " ";
+            check = true;
+        }
+    }
+
+    if (!check) cout << -1;
+    return 0;
+}
