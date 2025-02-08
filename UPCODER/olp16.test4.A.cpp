48ed3db954e8fe73d7cacc2cb793f12f5b30bfa9
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 13:06:58 +0700

    SEQUENCE

---
+++ UPCODER/olp16.test4.A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define MAXN 100005
+
+int n, x, ans;
+int a[MAXN], b[MAXN];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+    
+    b[1] = a[1];
+    for (int i = 2; i <= n; i++) 
+        b[i] = max(b[i-1], a[i-1]);
+    
+    x = ans = INT_MIN;
+    for (int i = 3; i <= n; i++) {
+        x = max(x, b[i-1] + 2 * a[i-1]);
+        ans = (ans, x + 3 * a[i]);
+    }
+    
+    cout << ans;
+    return 0;
+}
