48cf603bfdb20e219239dcab33041d97c042e32a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 16:02:35 +0700

    Cộng 2 phân số

---
+++ UPCODER/PhanSo1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+struct PhanSo {
+    int tu, mau;
+} a, b, res;
+
+int main() {
+    cin >> a.tu >> a.mau;
+    cin >> b.tu >> b.mau;
+
+    if (a.mau == 0 || b.mau == 0) cout << -1;
+    else {
+        res.tu = a.tu*b.mau + b.tu*a.mau;
+        res.mau = a.mau * b.mau;
+        int ucln = __gcd(res.tu, res.mau);
+        res.tu /= ucln;
+        res.mau /= ucln;
+        cout << res.tu << "/" << res.mau;
+    }
+    
+    return 0;
+}
