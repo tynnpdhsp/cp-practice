ab41d161c334c2e757b19bf71b0ba5c1aecd1ea0
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-08 16:24:56 +0700

    Tính tổng

---
+++ UPCODER/Tinh tong.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    double n, tong = 1, tich = 1;
+    cin >> n;
+
+    for (int i = 0; i <= n; i++) {
+        tich = 1;
+        for (int j = 0; j <= i; j++)
+            tich *= (double) 2*(j+1) / (2*j+3);
+        tong += tich;
+    }
+    
+    cout << fixed << setprecision(2) << tong;
+    return 0;
+}
