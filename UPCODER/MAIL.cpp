cf5ce96707cdeaa731b3403e3aabb5667cb4a34d
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-09 22:06:20 +0700

    Lọc mail

---
+++ UPCODER/MAIL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string tmp;
+    
+    while (cin >> tmp) {
+        size_t pos = tmp.find("@gmail.com");
+        if (pos != string::npos) 
+            cout << tmp << endl;
+    }
+    
+    return 0;
+}
