eb1078941d347716d49768a86fcd53f9dd06bd77
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-10 18:29:53 +0700

    Kiểm tra số tự mãn

---
+++ UPCODER/NARCISSISTICNUMBER.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string n;    
+    int sum, len, a[10][10];
+
+    for (int j = 0; j <= 9; j++)
+        a[1][j] = j;
+
+    for (int i = 2; i <= 9; i++) 
+        for (int j = 0; j <= 9; j++)
+            a[i][j] = a[i-1][j] * j;
+
+
+    while (cin >> n) {
+        sum = 0;
+        len = n.length();
+        for (int i = 0; i < len; i++)
+            sum += a[len][n[i]-48];
+        cout << (sum == stoi(n));
+    }
+
+    return 0;
+}
