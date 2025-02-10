45034919ae57919d14f4f56579e61cfe09f2ea14
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 00:23:35 +0700

    C11PRIME - Số nguyên tố

--- ONLINE JUDGE/SPOJ/C11PRIME.cpp
+++ ONLINE JUDGE/SPOJ/C11PRIME.cpp
@@ -1,23 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define endl '\n'
-#define int long long
-#define inp freopen("file.inp", "r", stdin)
-#define out freopen("file.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(0); cin.tie(0)
-
-const int MAXN = 1e6 + 5;
-const int MOD = 1e9 + 7;
-
+ 
+#define int int64_t
+ 
 bool prime(int n) {
     for (int i = 2; i*i <= n; i++)
         if (n % i == 0)
             return false;
     return n > 1;
 }
-
+ 
 bool solve() {
     int n;
     cin >> n;
@@ -25,7 +17,10 @@ bool solve() {
     for (int q = 2; q < 64; q++) {
         int p = round(exp(log(n)/q));
         int res = 1;
-        for (int i = 0; i < q; i++) res *= p;
+ 
+        for (int i = 0; i < q; i++) 
+            res *= p;
+ 
         if (prime(p) && res == n) {
             cout << p << " " << q;
             return true;
@@ -34,9 +29,8 @@ bool solve() {
     
     return false;
 }
-
-signed main() {
-    fastIO;
+ 
+int32_t main() {
     if (!solve()) cout << 0;
     return 0;
-}
+} 
