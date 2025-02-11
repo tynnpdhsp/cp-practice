b87f6e9319357fc0086d68855c1353ce3e43f5fb
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 22:49:54 +0700

    Mã BCD

--- UPCODER/BCD_Code.cpp
+++ UPCODER/BCD_Code.cpp
@@ -1,19 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n;
-    string a, b, c;
-    
-    cin >> a >> b >> c >> n;
-    int x = stoi(a, nullptr, 2);
-    int y = stoi(b, nullptr, 2);
-    int z = stoi(c, nullptr, 2);
+int n, num;
+string s[11];
 
-    if (n == x || n == y || n == z) 
-        cout << "YES";
-    else 
-        cout << "NO";
-    
+bool check() {
+    for (int i = 0; i < n; i++) {
+        int x = stoi(s[i], nullptr, 2);
+        if (num == x) return true;
+    }
+
+    return false;
+}
+
+int main() {
+    string line;
+    getline(cin, line);
+
+    stringstream ss(line);
+    while (ss >> s[n]) n++;
+    cin >> num;
+
+    cout << (check() ? "YES" : "NO");
     return 0;
 }
