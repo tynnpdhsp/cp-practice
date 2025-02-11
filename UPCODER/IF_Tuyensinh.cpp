bf63e026491688bda3f91bd8e17f4c88993e4463
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 22:40:03 +0700

    Xét điểm tuyển sinh

---
+++ UPCODER/IF_Tuyensinh.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double diemChuan, mon1, mon2, mon3, diemtb;
+    int doiTuong; 
+    char khuVuc;
+
+    cin >> diemChuan >> mon1 >> mon2 >> mon3;
+    cin >> khuVuc >> doiTuong;
+    diemtb = (mon1 + mon2 + mon3);
+
+    switch (doiTuong) {
+        case 1: diemtb += 2.5; break;
+        case 2: diemtb += 1.5; break;
+        case 3: diemtb += 1.0; break;
+    }
+
+    switch (khuVuc) {
+        case 'A': diemtb += 2.0; break;
+        case 'B': diemtb += 1.0; break;
+        case 'C': diemtb += 0.5; break;
+    }
+
+    if (diemtb >= diemChuan && mon1 > 0 && mon2 > 0 && mon3 > 0) cout << "PASS";
+    else cout << "FAIL";
+    return 0;
+}   
