635e75e4b01d7e38edd04943101664eb74de96b5
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 09:57:58 +0700

    Đếm Ký Tự

---
+++ UPCODER/CHAR.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    set<char> ans;
+    
+    getline(cin, s);
+    for (char c : s)
+        ans.insert(c);
+
+    cout << ans.size();
+    return 0;
+}
