705779dd5908e59eee79aba9a65db7c12b9c3071
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 21:15:41 +0700

    Tính tổng

---
+++ UPCODER/NHAPXUAT8.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+    cout << fixed << setprecision(1) << (a+b+c)/(d+e+f);
+    return 0;
+}
