b6f6235d0f38f12743dff346a6ea43cd46d4f39a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-11 16:23:06 +0700

    Xuất cơ bản

---
+++ UPCODER/CBXUAT.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "EM XIN HUA: KHONG BAO GIO\n";
+    cout << "COPY BAI (hoac chia se bai)\n";
+    cout << "NEU VI PHAM SE BI 0 DIEM TOAN BAI THI";
+    return 0;
+}
