b0e3b6fa542fddd93cfdd57005cbfbed9b90e96b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-12 17:28:40 +0700

    Số lớn nhất

---
+++ UPCODER/MAXNUM.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string n;
+int pos[10];
+vector<char> v;
+
+int main() {
+    cin >> n;
+    
+    for (int i = 0; i < n.size(); i++) 
+        pos[n[i] - '0'] = i;
+
+    bool swaped;
+    for (int i = 0; i < n.size(); i++) {
+        int cur = n[i] - '0';
+
+        for (int j = 9; j > cur; j--) {
+            if (pos[j] > i) {
+                swap(n[i], n[pos[j]]);
+                swaped = true;
+                break;
+            }
+        }
+
+        if (swaped) break;
+    }
+
+    cout << n;
+    return 0;
+}
