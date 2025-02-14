421c29f31ea6767ea92183ae0d6ae76a41153c4e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-14 18:28:44 +0700

    Dãy Fibonacci

--- UPCODER/DHLTNC.QHĐ.7.1.cpp
+++ UPCODER/DHLTNC.QHĐ.7.1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n) {
-    if (n == 1 || n == 2) return 1;
-    return fib(n - 1) + fib(n - 2);
-}
+int n, dp[101];
 
 int main() {
-    int n;
     cin >> n;
-    cout << fib(n);
+    
+    dp[1] = dp[2] = 1;
+    for (int i = 3; i <= n; i++)
+        dp[i] = dp[i-1] + dp[i-2];
+        
+    cout << dp[n];
     return 0;
 }
