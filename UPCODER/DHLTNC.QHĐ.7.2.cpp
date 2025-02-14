85450a4238efebfa7eb00f6a4656e2d1496c40cd
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-14 18:27:14 +0700

    Dãy con đơn điệu dài nhất

---
+++ UPCODER/DHLTNC.QHĐ.7.2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ans;
+int a[1001], dp[1001];
+
+int main() {
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+        cin >> a[i];
+        
+    fill(dp, dp + 1001, 1);
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = i-1; j > 0; j--) 
+            if (a[j] < a[i] && dp[j] >= dp[i])
+                dp[i] = dp[j] + 1;
+        ans = max(ans, dp[i]);
+    }
+    
+    cout << ans;
+    return 0;
+}
