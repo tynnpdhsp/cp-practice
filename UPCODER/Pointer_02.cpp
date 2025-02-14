3cef2d1d9233c25c1ed04b7d9b5d37a01bc2f9ab
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-14 23:08:04 +0700

    Con trỏ 02

---
+++ UPCODER/Pointer_02.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int m = 29;
+    cout << "Address of m: " << &m << '\n';
+    cout << "Value of m: " << m << '\n';
+    
+    int* ab = &m;
+    cout << "Now ab is assigned with the address of m.\n";
+    cout << "Address of pointer ab: " << ab << '\n';
+    cout << "Content of pointer ab: " << *ab << '\n';
+    
+    m = 34;
+    cout << "The value of m assigned to 34 now.\n";
+    cout << "Address of pointer ab: " << ab << '\n';
+    cout << "Content of pointer ab: " << *ab << '\n';
+    
+    *ab = 7;
+    cout << "Address of m: " << &m << '\n';
+    cout << "Value of m: " << m << '\n';
+    
+    return 0;
+}
