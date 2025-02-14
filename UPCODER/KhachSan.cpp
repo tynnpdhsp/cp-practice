3768f04d1d6351d2861686e3e2d684f3b6adbb04
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-14 21:17:04 +0700

    Khach Sạn

---
+++ UPCODER/KhachSan.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, res = 0;
+    cin >> a >> b;
+
+    if (a < 1 || a > 7 || b < 1 || b > 7) cout << "NHAP SAI";
+    else {
+        if (a == b) res = 0;
+        else if (a == 1) res = 400000 + (b-a-1)*300000;
+        else if (a > b && b == 1) res = (7-a+b)*300000;
+        else if (a > b) res = 400000 + (6-a+b)*300000;
+        else res = (b-a) * 300000;
+        cout << res;
+    }
+
+    return 0;
+}
