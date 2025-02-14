91fedc88047568cd22937e0ecd43bea98833d379
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-14 17:08:26 +0700

    Sắp xếp

---
+++ UPCODER/BT_Cơ_bản4.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n];
+    for (int &x : a) cin >> x;
+
+    sort(a, a + n);
+    for (int x : a) cout << x << ' ';
+
+    return 0;
+}
