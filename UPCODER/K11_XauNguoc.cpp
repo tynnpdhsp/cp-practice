61fed8048d86087a1403c652225d114b4cbba3d6
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-16 21:20:07 +0700

    Xuất xâu ngược

---
+++ UPCODER/K11_XauNguoc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    for (int i = s.size()-1; i >= 0; i--)
+        cout << s[i];
+
+    return 0;
+}
