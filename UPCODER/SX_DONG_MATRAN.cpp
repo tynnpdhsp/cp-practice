9b8745bfb78e53dc816bffb7d7d925fec96d7cc8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-17 13:04:10 +0700

    Sắp xếp dòng ma trận

---
+++ UPCODER/SX_DONG_MATRAN.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1000 + 5;
+
+int n, m;
+int a[MAXN][MAXN];
+int ans[MAXN][MAXN];
+pair<int, int> r[MAXN];
+
+signed main() {
+    fastIO;
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+        r[i].first = 1;
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+            r[i].first *= a[i][j];
+            r[i].second = i;
+        }
+    }
+
+    sort(r, r + n);
+
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j < m; j++)
+            ans[i][j] = a[r[i].second][j];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) 
+            cout << ans[i][j] << ' ';
+        cout << endl;
+    }
+
+    return 0;
+}
