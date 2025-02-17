a50f3d2ff04c90b1657b8b9cda86c30aba52c460
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-17 12:26:38 +0700

    KT45P1

---
+++ UPCODER/KT45P1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    cout << a + b << endl;
+    cout << a - b << endl;
+    cout << a * b << endl;
+    cout << a / b << endl;
+    cout << a % b << endl;
+    cout << a * 3.14 << endl;
+    cout << 3.14 * a * a / 4.0 << endl;
+    cout << (c + b) * 2 << endl;
+    cout << c * b << endl;
+    
+    return 0;
+}
