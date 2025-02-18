feaee6b4e6cd46fc1f89e9c56f8489e844e8caf2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-18 08:30:27 +0700

    ABC171.BaiA

---
+++ UPCODER/ABC171.BaiA.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+
+    if (islower(c))
+        cout << 'a';
+    else 
+        cout << 'A';
+
+    return 0;
+}
