433cb5a3258356c8b6017fcb7ae0fbf06460466a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-20 21:41:08 +0700

    Tăng số

---
+++ UPCODER/TĂNGSỐ.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    
+    for (int i = 0; i < 3; i++) {
+        if (s[i] == '9') s[i] = '0';
+        else s[i] = s[i] + 1;
+    }
+
+    cout << str;
+    return 0;
+}
