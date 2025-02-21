36d34bcabc275f684330def2adc84df8603d07c8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-02-21 22:59:05 +0700

    Xác định số nguyên tố

---
+++ UPCODER/LTCB-C3-01.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool prime(int n) {
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0)
+            return false;
+    return n > 1;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << prime(n);
+    return 0;
+}
