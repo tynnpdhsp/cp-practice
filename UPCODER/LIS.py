e7c2190bb3a015f25c2dc5c9c413bcb74121757c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-04-28 12:16:54 +0700

    Dãy con tăng dài nhất

---
+++ UPCODER/LIS.py
@@ -0,0 +1,10 @@
+a = list(map(int, input().split()))
+n = len(a)
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if a[j] < a[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(max(dp), end='')
