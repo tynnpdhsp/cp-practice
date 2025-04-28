9a8bd515e3cdf518f1b7ab88a3332c27f9f8f04e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-04-28 16:15:46 +0700

    Update MINIGAME18.2-TACHSO.cpp

--- UPCODER/MINIGAME18.2-TACHSO.cpp
+++ UPCODER/MINIGAME18.2-TACHSO.cpp
@@ -1,23 +1,21 @@
 #include <iostream>
 #include <sstream>
-#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main() {
-    string s; 
-    vector<string> st; 
-    int k = 0;
+    string s;
+    cin >> s;
+    replace(s.begin(), s.end(), ',', ' ');
     
-    getline(cin, s);
-    istringstream iss(s);
-
-    while (getline(iss, s, ','))
-        st.push_back(s);
-
-    int ans = 0;
-    for (auto x : st) 
-        ans += stoi(x), cout << x << endl; 
+    int x, sum = 0;
+    stringstream ss(s);
     
-    cout << ans;
+    while (ss >> x) {
+        cout << x << '\n';
+        sum += x;
+    }
+    
+    cout << sum;
     return 0;
 }
