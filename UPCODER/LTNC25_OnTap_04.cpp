14bcc7976539577711d51446e317974345001e1a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-05-18 19:10:06 +0700

    Kiểm tra tọa độ

---
+++ UPCODER/LTNC25_OnTap_04.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+struct ToaDo {
+    double x, y;
+};
+
+istream &operator >> (istream &in, ToaDo &td) {
+    in >> td.x >> td.y;
+    return in;
+}
+
+ostream &operator << (ostream &out, ToaDo td) {
+    out << fixed << setprecision(2);
+    out << '(' << td.x << ", " << td.y << ')';
+    return out;
+}
+
+ToaDo operator + (ToaDo a, ToaDo b) {
+    ToaDo res;
+    res.x = a.x + b.x;
+    res.y = a.y + b.y;
+    return res;
+}
+
+ToaDo operator - (ToaDo a, ToaDo b) {
+    ToaDo res;
+    res.x = a.x - b.x;
+    res.y = a.y - b.y;
+    return res;
+}
+
+bool operator > (ToaDo a, ToaDo b) {
+    if (a.x > b.x)
+        return true;
+    
+    if (a.x == b.x && a.y > b.y)
+        return true;
+    
+    return false;
+}
+
+bool operator < (ToaDo a, ToaDo b) {
+    if (a.x < b.x) 
+        return true;
+
+    if (a.x == b.x && a.y < b.y)
+        return true;
+    
+    return false;
+}
+
+bool operator == (ToaDo a, ToaDo b) {
+    return (a.x == b.x && a.y == b.y);
+}
+
+int main() {
+    ToaDo a, b;
+    cin >> a >> b;
+
+    if (a < b)
+        cout << "nho hon";
+    else if (a > b)
+        cout << "lon hon";
+    else 
+        cout << "trung nhau";
+
+    return 0;
+}
