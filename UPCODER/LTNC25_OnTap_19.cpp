c91ec652257ee1b8d2310e1ef01f689192bcc95d
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-05-18 18:55:20 +0700

    Đổi số thập phân sang nhị phân

---
+++ UPCODER/LTNC25_OnTap_19.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    stack<int> st;
+
+    if (n == 0) {
+        cout << 0 << endl;
+        return 0;
+    }
+
+    while (n > 0) {
+        st.push(n % 2);
+        n /= 2;
+    }
+
+    while (!st.empty()) {
+        cout << st.top();
+        st.pop();
+    }
+
+    cout << endl;
+    return 0;
+}
