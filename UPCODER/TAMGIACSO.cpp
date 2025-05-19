17e8f8870b3417c98ff98541322585f60271216b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-05-20 03:13:21 +0700

    Update TAMGIACSO.cpp

--- UPCODER/TAMGIACSO.cpp
+++ UPCODER/TAMGIACSO.cpp
@@ -1,54 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, sum, ans;
-int a[100][100];
-vector<int> ketQua, duongDi;
+int sum, sumMax;
+int n, a[100][100];
+vector<int> path, pathRes;
 
 int dx[] = {1, 1};
 int dy[] = {0, 1};
 
-void Try(int x, int y) {
-    if (x == n-1) {
-        if (ans < sum) {
-            ans = sum;
-            ketQua = duongDi;
+void Try(int i, int j) {
+    sum += a[i][j];
+    path.push_back(a[i][j]);
+    
+    if (i == n - 1) {
+        if (sum > sumMax) {
+            sumMax = sum;
+            pathRes = path;
         }
+        
         return;
-    } 
-     
-    for (int i = 0; i < 2; i++) {
-        int xx = x + dx[i];
-        int yy = y + dy[i];
+    }
+    
+    for (int k = 0; k < 2; k++) {
+        int x = i + dx[k];
+        int y = j + dy[k];
+        
+        if (y <= x) {
+            Try(x, y);
             
-        if (xx >= 0 && xx < n && yy >= 0 && yy <= xx) {
-            sum += a[xx][yy];
-            duongDi.push_back(a[xx][yy]);
-                
-            Try(xx, yy);
-                
-            sum -= a[xx][yy];
-            duongDi.pop_back();
+            sum -= a[x][y];
+            path.pop_back();
         }
-    }  
+    }
 }
 
 int main() {
     cin >> n;
     
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j <= i; j++) 
+    for (int i = 0; i < n; i++) 
+        for (int j = 0; j <= i; j++)
             cin >> a[i][j];
-    
-    ans = INT_MIN;
-    sum = a[0][0];
-    duongDi.push_back(a[0][0]);
+ 
     Try(0, 0);
     
-    for (int x : ketQua) 
-        cout << x << " ";
+    for (int i = 0; i < pathRes.size(); i++)
+        cout << pathRes[i] << ' ';
     cout << endl;
     
-    cout << ans;
+    cout << sumMax;
     return 0;
 }
