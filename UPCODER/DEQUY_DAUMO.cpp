1ad0a6d1b6f5510f0de34f99c5fb82d1f756c2d4
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-05-19 21:59:58 +0700

    Update DEQUY_DAUMO.cpp

--- UPCODER/DEQUY_DAUMO.cpp
+++ UPCODER/DEQUY_DAUMO.cpp
@@ -3,55 +3,63 @@
 using namespace std;
 
 struct ToaDo {
-    int first, second;
-    ToaDo(int a, int b): first(a), second(b) {}
+    int x, y;
+    ToaDo(int a, int b): x(a), y(b) {}
 };
 
-int m, n, dem, ans;
-int a[100][100];
+int m, n, dem, MAX;
+int maTran[100][100];
 vector<ToaDo> ketQua, duongDi;
 
 int dx[] = {1, 0};
 int dy[] = {0, 1};
+int dp[100][100];
 
 void Try(int x, int y) {
+    if (dem <= dp[x][y]) return;  
+    dp[x][y] = dem;
+
     if (x == m && y == n) {
-        if (dem > ans) {
-            ans = dem;
+        if (dem > MAX) {
+            MAX = dem;
             ketQua = duongDi;
         }
         return;
     } 
-    
+
     for (int i = 0; i < 2; i++) {
         int xx = x + dx[i];
         int yy = y + dy[i];
         if (xx >= 1 && xx <= m && yy >= 1 && yy <= n) {
             duongDi.push_back({xx, yy});
-            dem += a[xx][yy];
+            dem += maTran[xx][yy];
                 
             Try(xx, yy);
                 
             duongDi.pop_back();
-            dem -= a[xx][yy];     
+            dem -= maTran[xx][yy];
         }
     }
 }
 
 int main() {
-    cin >> m >> n;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
     
+    cin >> m >> n;
+    memset(dp, -1, sizeof(dp));
+
     for (int i = 1; i <= m; i++)
         for (int j = 1; j <= n; j++) 
-            cin >> a[i][j];
+            cin >> maTran[i][j];
             
-    ans = INT_MIN;
-    dem = a[1][1];
+    MAX = INT_MIN;
+    dem = maTran[1][1];
     duongDi.push_back({1, 1});
     Try(1, 1);
     
-    cout << ans << endl;
+    cout << MAX << endl;
     for (const auto &x : ketQua)
-        cout << x.first << " " << x.second << endl;
+        cout << x.x << " " << x.y << endl;
     return 0;
 }
