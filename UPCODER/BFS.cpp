676526965e1399a89b85ebe40acd4b5687715554
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-02 20:24:09 +0700

    Duyệt theo chiều rộng

--- UPCODER/BFS.cpp
+++ UPCODER/BFS.cpp
@@ -1,41 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int N, M, S;
 const int MAXN = 1e5 + 5;
+int n, m, s;
 vector<int> adj[MAXN];
 bool visited[MAXN];
 
-void BFS(int u) {
+void bfs(int u) {
     queue<int> q;
     q.push(u);
     visited[u] = true;
-
+    
+    int x;
     while (!q.empty()) {
-        int v = q.front();
+        x = q.front();
         q.pop();
-
-        cout << v << ' ';
-
-        for (int x : adj[v]) {
-            if (!visited[x]) {
-                q.push(x);
-                visited[x] = true;
+        
+        cout << x << ' ';
+        
+        for (int v : adj[x]) {
+            if (!visited[v]) {
+                q.push(v);
+                visited[v] = true;
             }
         }
     }
 }
 
 int main() {
-    cin >> N >> M >> S;
-
-    while (M--) {
-        int u, v;
+    cin >> n >> m >> s;
+    
+    int u, v;
+    while (m--) {
         cin >> u >> v;
         adj[u].push_back(v);
         adj[v].push_back(u);
     }
-
-    BFS(S);
+    
+    bfs(s);
     return 0;
 }
