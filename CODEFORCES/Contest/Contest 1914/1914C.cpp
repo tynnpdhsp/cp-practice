cc593e776a4d200a1d5e2fdb092f40173f3b9fe7
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-13 22:01:41 +0700

    C. Quests

---
+++ CODEFORCES/Contest/Contest 1914/1914C.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+int t, n, k, ans;
+int a[MAXN], b[MAXN];
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> t;
+
+    while (t--) {
+        ans = 0;
+        cin >> n >> k;
+
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+            a[i] += a[i - 1];
+        }
+
+        for (int i = 1; i <= n; i++) {
+            cin >> b[i];
+            b[i] = max(b[i], b[i-1]);
+        }
+
+        for (int i = 1; i <= min(n, k); i++) 
+            ans = max(ans, a[i] + b[i] * (k - i));
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
