7dfdc984e15eda7a8d88316e59679d6b7e51e3dc
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-14 11:07:45 +0700

    Problem 3. It's All About the Base

---
+++ USACO/509.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MIN_BASE = 10;
+const int MAX_BASE = 15'000;
+
+int t, cur, y;
+string a, b;
+
+int toBase10(int b, string n) {
+    return (n[0] - '0') * b * b + (n[1] - '0') * b + (n[2] - '0');
+}
+
+int main() {
+    freopen("whatbase.in", "r", stdin);
+    freopen("whatbase.out", "w", stdout);
+    cin >> t;
+    
+    while (t--) {
+        cin >> a >> b;
+
+        y = MIN_BASE;
+        for (int x = MIN_BASE; x <= MAX_BASE; x++) {
+            cur = toBase10(x, a);
+
+            while (toBase10(y, b) < cur) y++;
+
+            if (y <= MAX_BASE && toBase10(y, b) == cur) {
+                cout << x << ' ' << y << '\n';
+                break;
+            }
+        } 
+    }
+
+    return 0;
+}
