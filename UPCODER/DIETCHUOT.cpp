a8e79f282ff61a902785e52e9648cad4960ec0de
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-19 10:12:30 +0700

    Diệt chuột

--- UPCODER/DIETCHUOT.cpp
+++ UPCODER/DIETCHUOT.cpp
@@ -1,38 +1,64 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int t, d, n, p, q, z;
-int a[1024][1024], k = -1;
+const int MAXN = 1025;
+int t, d, n, p, q, k;
+int c[MAXN][MAXN], s[MAXN][MAXN];
 
 int main() {
     cin.tie(0) -> sync_with_stdio(0);
+
     cin >> t;
-    
     while (t--) {
-        for (int i = 0; i < 1024; i++)
-            for (int j = 0; j < 1024; j++)
-                a[i][j] = 0;
-
         cin >> d >> n;
-        for (int i = 0; i < n; i++) {
-            cin >> p >> q >> z;
+        memset(c, 0, sizeof(c));
 
-            for (int a = p - d; a <= p + d; a++) {
-                if (a < 0 || a > 1023) continue;
+        int x, y, z;
+        for (int i = 0; i < n; ++i) {
+            cin >> x >> y >> z;
+            c[x][y] += z;
+        }
 
-                for (int b = q - d; b <= q + d; b++) {
-                    if (b < 0 || b > 1023) continue;
-                    a[a][b] += z;
+        for (int i = 0; i < MAXN; ++i) {
+            for (int j = 0; j < MAXN; ++j) {
+                s[i][j] = c[i][j];
+
+                if (i > 0) 
+                    s[i][j] += s[i - 1][j];
+
+                if (j > 0) 
+                    s[i][j] += s[i][j - 1];
+
+                if (i > 0 && j > 0)     
+                    s[i][j] -= s[i - 1][j - 1];
+            }
+        }
+
+        p = 0, q = 0, k = 0;
+        for (int x = 0; x < MAXN; ++x) {
+            for (int y = 0; y < MAXN; ++y) {
+                int x1 = max(0, x - d), y1 = max(0, y - d);
+                int x2 = min(1024, x + d), y2 = min(1024, y + d);
+                int sum = s[x2][y2];
+
+                if (x1 > 0) 
+                    sum -= s[x1 - 1][y2];
+
+                if (y1 > 0) 
+                    sum -= s[x2][y1 - 1];
+                
+                if (x1 > 0 && y1 > 0) 
+                    sum += s[x1 - 1][y1 - 1];
+
+                if (sum > k || (sum == k && (x < p || (x == p && y < q)))) {
+                    k = sum;
+                    p = x;
+                    q = y;
                 }
             }
         }
 
-        for (int i = 0; i < 1024; i++)
-            for (int j = 0; j < 1024; j++)
-                if (a[i][j] > k) 
-                    p = i, q = j, k = a[i][j];
-        
-        cout << p << " " << q << " " << k;
+        cout << p << ' ' << q << ' ' << k << '\n';
     }
 
     return 0;
