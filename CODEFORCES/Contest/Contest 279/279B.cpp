a09c098725dba6e7b6dc46bcc51875c2d7046c29
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-23 16:38:36 +0700

    B. Books

--- CODEFORCES/Contest/Contest 279/279B.cpp
+++ CODEFORCES/Contest/Contest 279/279B.cpp
@@ -1,41 +1,26 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-using namespace __gnu_pbds;
 
-#define endl '\n'
-#define int long long
-#define inp freopen("file.inp", "r", stdin)
-#define out freopen("file.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
-template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
-template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
-
+#define int int64_t 
 const int MAXN = 1e5 + 5;
-const int MOD = 1e9 + 7;
 
 int n, t, ans;
 int a[MAXN];
 
-signed main() {
-    fastIO;
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> n >> t;
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++)
         cin >> a[i];
     
-    int i = 1, j = 1, sum = 0;
-
-    while (i <= n && j <= n) {
-        sum += a[i++];
-        
-        while (sum > t)
-            sum -= a[j++];
-        
-        ans = max(ans, i - j);
+    int i = 0, j = 0, sum = 0;
+    
+    while (i < n && j < n) {
+        sum += a[j++];
+        while (sum > t) 
+            sum -= a[i++];
+        ans = max(ans, j - i);
     }
     
     cout << ans;
