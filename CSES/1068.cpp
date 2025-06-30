f7670b813edfba142ef8f9516abf5806b112cccd
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-30 14:26:13 +0700

    Weird Algorithm

--- CSES/1068.cpp
+++ CSES/1068.cpp
@@ -1,19 +1,23 @@
-#include <iostream>
+/*********************************************************************************************************** 
+ * Yêu cầu: Biến đổi n. Nếu n chẵn => n = n / 2; n lẽ => n = 3 * n + 1.
+ * Giải: Cài đặt theo yêu cầu đề. Trường hợp khi n = 1 không dừng sẽ bị lặp vô tận. Có thể tràn số.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    long long n;
+    int64_t n;
     cin >> n;
-    
+
     while (n != 1) {
         cout << n << ' ';
 
         if (n & 1) 
-            n = 3*n + 1;
+            n = 3 * n + 1;
         else 
-            n /= 2;
+            n = n / 2;
     }
 
-    cout << n;
+    cout << 1;
     return 0;
 }
