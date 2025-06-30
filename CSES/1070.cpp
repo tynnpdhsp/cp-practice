9a3b22e914740f14d0aab7939f4b69d6f735309d
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-30 15:11:10 +0700

    Number Spiral

--- CSES/1070.cpp
+++ CSES/1070.cpp
@@ -1,14 +1,19 @@
-#include <iostream>
+/*********************************************************************************************************** 
+ * Yêu cầu: Cho số n, hoán vị dãy 1 .. n sao cho không có số nào liền kề chênh nhau 1 đơn vị.
+ * Giải: 
+ *  Trường hợp 1 < n < 4 không có hoán vị nào phù hợp.
+ *  In số chẳn trước, số lẻ sau. Không làm ngược lại, vì trường hợp n = 4 => 1 3 2 4, sẽ bị sai.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
+int n;
 
-    int n;
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
 
-    if (n == 2 || n == 3) {
+    if (n > 1 && n < 4) {
         cout << "NO SOLUTION";
         return 0;
     }
@@ -18,6 +23,6 @@ int main() {
     
     for (int i = 1; i <= n; i += 2)
         cout << i << ' ';
-
+    
     return 0;
 }
