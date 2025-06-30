45776257fa3939099553a58a4dd4a5fb88e59926
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-06-30 16:29:45 +0700

    Number Spiral

--- CSES/1071.cpp
+++ CSES/1071.cpp
@@ -1,13 +1,16 @@
 /*********************************************************************************************************** 
  * Yêu cầu: Truy vấn giá trị tại ô (x, y) trong xoắn ốc vô hạn.
+
  * Giải: 
  *  Trong hình vuông cấp k chứa giá trị lớn nhất là k^2.
  *  Gọi k = max(x, y) - 1, giá trị cần tìm nằm ngoài hình vuông cấp k.
  *  Cần tìm khoảng cách từ k^2 đến ô (x, y)
+
  *  Đối với trường hợp x >= y, ta xét theo x: 
  *      Nếu x lẻ: cột có giá trị giảm từ trên xuống => k^2 + 2x - y 
  *      Nếu x chẵn: cột có giá trị tăng từ trên xuống => k^2 + y
- * Đối với trường hợp x < y, ta xét theo y:
+
+ *  Đối với trường hợp x < y, ta xét theo y:
  *      Nếu y lẻ: hàng có giá trị tăng từ trái qua phải => k^2 + x
  *      Nếu y chẵn: hàng có giá trị giảm từ trái qua phải => k^k + 2y - x
 ***********************************************************************************************************/
