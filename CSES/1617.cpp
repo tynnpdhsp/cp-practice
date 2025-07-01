88f6072bcd296e24ff726d3623f2294a7da61855
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-01 14:30:31 +0700

    Bit Strings

--- CSES/1617.cpp
+++ CSES/1617.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+/*********************************************************************************************************** 
+ * Yêu cầu: Tính số lượng xâu nhị phân có độ dài n.
+ * Giải: Số lượng xâu nhị phân có độ dài n là 2^n. Sử dụng tính chất a^b mod m = ((a mod m)^(b mod m)) mod m
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
+const int MOD = 1e9 + 7;
+int64_t n, ans = 1;
+
 int main() {
-    const int MOD = 1e9 + 7;
-    long long n, ans = 1;
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
 
     for (int i = 1; i <= n; i++) 
