01b67878d294014329b6107a6b8f7faf3c954c6a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-01 15:23:08 +0700

    Creating Strings

--- CSES/1622.cpp
+++ CSES/1622.cpp
@@ -1,61 +1,24 @@
-// #include <iostream>
-// #include <set>
-// using namespace std;
-
-// set<string> ans;
-// bool chosen[10];
-// string s, permutation;
-
-// void Try() {
-//     if (permutation.size() == s.size()) {
-//         ans.insert(permutation);
-//         return;
-//     } 
-
-//     for (int i = 0; i < s.size(); i++) {
-//         if (chosen[i])  
-//             continue;
-
-//         chosen[i] = true;
-//         permutation.push_back(s[i]);
-//         Try();
-//         chosen[i] = false;
-//         permutation.pop_back();
-//     }
-// }
-
-// int main() {
-//     cin.tie(0) -> ios_base::sync_with_stdio(0);
-//     cin >> s;
-//     Try();
-
-//     cout << ans.size() << '\n';
-//     for (string x : ans)
-//         cout << x << '\n';
-    
-//     return 0;
-// }
-
-#include <iostream>
-#include <set>
-#include <algorithm>
+/*********************************************************************************************************** 
+ * Yêu cầu: Sinh tất cả chuỗi hoán vị của chuỗi đầu vào.
+ * Giải: Dùng hàm next_permutation
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
 string s;
-set<string> ans;
+vector<string> v;
 
 int main() {
-    cin.tie(0) -> ios_base::sync_with_stdio(0);
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> s;
 
     sort(s.begin(), s.end());
-
     do {
-        ans.insert(s);
+        v.push_back(s);
     } while (next_permutation(s.begin(), s.end()));
-    
-    cout << ans.size() << '\n';
-    for (string x : ans)
+
+    cout << v.size() << '\n';
+    for (string x : v)
         cout << x << '\n';
     
     return 0;
