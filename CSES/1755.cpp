3c4a56f06b88048c742e894e7fda6db1167efcb7
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-01 14:57:06 +0700

    Palindrome Reorder

--- CSES/1755.cpp
+++ CSES/1755.cpp
@@ -1,41 +1,40 @@
-#include <iostream>
-#include <string>
+/*********************************************************************************************************** 
+ * Yêu cầu: Từ chuỗi ban đầu, tạo thành chuỗi đối xứng. Nếu tạo không được thì in "NO SOLUTION".
+ * Giải: 
+ *  Đếm số lượng ký tự, nếu xuất hiện 2 ký tự có tần số xuất hiện lẻ thì in "NO SOLUTION". 
+ *  Chèn ký tự đúng với tần số xuất hiện vào giữa chuỗi để tạo chuỗi đối xứng, chèn ký tự có tần số chẵn trước.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
-char odd;
 string s, ans;
-int a[1001];
+int a[300], cntLe;
+char ktLe;
 
 int main() {
-    ios_base::sync_with_stdio(0);
-    cin.tie(0);
-
+    cin.tie(0) -> sync_with_stdio(0);
     cin >> s;
-    for (char &c : s)
-        a[c]++; 
 
-    int cntOdd = 0;
-    for (char c = 'A'; c <= 'Z'; c++) {
-        if (a[c] & 1) {
-            cntOdd++;
-            odd = c;
-            continue;
+    for (char c : s) a[c]++;
+    for (int i = 0; i < 300; i++) {
+        if (a[i] & 1) {
+            cntLe++;
+            ktLe = i;
         }
-        
-        if (a[c])
-            ans.insert(ans.size() / 2, a[c], c);
-
-        if (cntOdd > 1) 
-            break;
     }
 
-    if (cntOdd)
-        ans.insert(ans.size() / 2, a[odd], odd);
-
-    if (cntOdd > 1) 
+    if (cntLe > 1) {
         cout << "NO SOLUTION";
-    else 
-        cout << ans;
-    
+        return 0;
+    }
+
+    for (int i = 0; i < 300; i++) 
+        if (a[i] > 0 && a[i] % 2 == 0) 
+            ans.insert(ans.size() / 2, a[i], i);
+
+    if (cntLe) 
+        ans.insert(ans.size() / 2, a[ktLe], ktLe);
+
+    cout << ans;
     return 0;
 }
