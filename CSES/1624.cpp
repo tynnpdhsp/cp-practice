4561b0285070a308bc7668fb74ae7335bcd4f057
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-02 11:53:41 +0700

    Chessboard and Queens

--- CSES/1624.cpp
+++ CSES/1624.cpp
@@ -1,16 +1,20 @@
-#include <iostream>
+/*********************************************************************************************************** 
+ * Yêu cầu: Tính cách đặt 8 quân hậu trên bàn cờ 8x8 sao cho chúng không ăn nhau.
+ * Giải: Cần đặt 8 quân hậu ở 8 dòng, xét từng dòng, dùng quay lui để xét tất cả trường hợp.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
 int ans;
 char c[8][8];
 bool visited[8][8];
 
-bool check(int x, int y) {
+bool check(int x, int y) {  
     if (c[x][y] == '*')
         return false;
-
-    for (int i = 0; i <= x; i++)
-        if (visited[i][y])
+    
+    for (int i = 0; i <= x; i++)    
+        if (visited[i][y]) 
             return false;
     
     for (int i = 0; i <= y; i++)
@@ -20,7 +24,7 @@ bool check(int x, int y) {
     for (int i = x, j = y; i >= 0 && j >= 0; i--, j--)
         if (visited[i][j])
             return false;
-
+    
     for (int i = x, j = y; i >= 0 && j < 8; i--, j++)
         if (visited[i][j])
             return false;
@@ -44,11 +48,12 @@ void Try(int x) {
 }
 
 int main() {
-    cin.tie(0) -> ios_base::sync_with_stdio(0);
-    for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++)
-            cin >> c[i][j];
+    cin.tie(0) -> sync_with_stdio(0);
     
+    for (int i = 0; i < 8; i++) 
+        for (int j = 0; j < 8; j++) 
+            cin >> c[i][j];
+
     Try(0);
     cout << ans;
     return 0;
