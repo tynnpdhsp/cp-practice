a8710cef36fd5472f334b4d6dc94e067d1c44cbf
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-02 16:20:48 +0700

    Static Range Sum Queries

--- CSES/1646.cpp
+++ CSES/1646.cpp
@@ -1,26 +1,31 @@
+/*********************************************************************************************************** 
+ * Yêu cầu: Truy vấn tổng đoạn [l, r] trên mảng. 
+ * Giải: 
+ *  Dùng kỹ thuật mảng cộng dồn.
+ *  Dùng kỹ thuật cây phân đoạn.
+***********************************************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
+#define int int64_t 
 const int MAXN = 2e5 + 5;
 
-int n, q;
-int a[MAXN], sum[MAXN];
+int n, q, a[MAXN];
+int l, r, sum[MAXN];
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n >> q;
-   
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++) 
         cin >> a[i];
+    
+    for (int i = 1; i <= n; i++)
         sum[i] = sum[i-1] + a[i];
-    }
-    
+
     while (q--) {
-        int l, r;
         cin >> l >> r;
-        cout << sum[r] - sum[l - 1] << '\n';
+        cout << sum[r] - sum[l-1] << '\n';
     }
-    
+
     return 0;
 }
