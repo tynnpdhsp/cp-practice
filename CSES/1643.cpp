31b508fa497063613445e5eb5f51a45f6640c718
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-02 15:30:26 +0700

    Maximum Subarray Sum

--- CSES/1643.cpp
+++ CSES/1643.cpp
@@ -1,47 +1,28 @@
+/*********************************************************************************************************** 
+ * Yêu cầu: Tìm đoạn con liên tiếp có tổng lớn nhất.
+ * Giải: Lần lượt duyệt qua a[i], sum = max(sum + a[i], a[i]).
+***********************************************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define MAXN 200005
+#define int int64_t 
+const int MAXN = 2e5 + 5;
 
-int n, ans;
-int a[MAXN], sum[MAXN];
-
-// int32_t main() {
-//     cin.tie(0) -> sync_with_stdio(0);
-//     cin >> n;
-    
-//     for (int i = 1; i <= n; i++) {
-//         cin >> a[i];
-//         sum[i] = sum[i-1] + a[i];
-//     }
-    
-//     int minVal = 0;
-//     ans = INT_MIN;
-    
-//     for (int i = 1; i <= n; i++) {
-//         ans = max(ans, sum[i] - minVal);
-//         minVal = min(minVal, sum[i]);
-//     }  
-   
-//     cout << ans;
-//     return 0;
-// }
-
+int n, a[MAXN];
+int sum, ans;
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
-  
-    int sum = 0;
-    ans = INT_MIN;
-
-    for (int i = 1; i <= n; i++) {
+    for (int i = 0; i < n; i++) 
         cin >> a[i];
-        sum = max(a[i], sum + a[i]);
+    
+    ans = sum = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        sum = max(a[i] + sum, a[i]);
         ans = max(ans, sum);
     }
-    
+
     cout << ans;
     return 0;
 }
