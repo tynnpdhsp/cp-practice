297370da0d70f3acf90f8a5a59978a82395c3bd0
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-02 12:45:49 +0700

    Sum of Two Values

--- CSES/1640.cpp
+++ CSES/1640.cpp
@@ -1,104 +1,32 @@
-// #include <iostream>
-// #include <set>
-// using namespace std;
-
-// int n, x, k;
-// bool found;
-// set<pair<int, int>> s;
-
-// int main() {
-//     cin.tie(0) -> sync_with_stdio(0);
-//     cin >> n >> k;
-    
-//     for (int i = 1; i <= n; i++) {
-//         cin >> x;
-//         s.insert({x, i});
-//     }
-    
-//     for (auto p : s) {
-//         int v = k - p.first;
-//         auto it = s.lower_bound({v, 1});
-        
-//         if (it != s.end() && it->first == v && it->second != p.second) {
-//             cout << p.second << ' ' << it->second;
-//             found = true;
-//             break;
-//         }
-//     }
-    
-//     if (!found)
-//         cout << "IMPOSSIBLE";
-//     return 0;
-// }
-    
-    
-    
-// #include <iostream>
-// #include <map>
-// using namespace std;
-
-// int n, x, k;
-// bool found;
-// map<int, int> m;
-
-// int main() {
-//     cin.tie(0) -> sync_with_stdio(0);
-//     cin >> n >> k;
-    
-//     for (int i = 1; i <= n; i++) {
-//         cin >> x;
-        
-//         if (m.count(k - x)) {
-//             cout << i << ' ' << m[k - x];
-//             found = true;
-//             break;
-//         }
-        
-//         m[x] = i;
-//     }
-  
-//     if (!found) 
-//         cout << "IMPOSSIBLE";
-//     return 0; 
-// }
-
-
-
-#include <iostream>
-#include <algorithm>
-#include <vector>
+/*********************************************************************************************************** 
+ * Yêu cầu: Tìm 2 vị trí có tổng bằng x
+ * Giải cách 1: Tìm x - a[i] đã xuất hiện trước đó hay chưa.
+ *  Tìm giá trị của x - a[i] xem đã xuất hiện trước đó chưa, nếu có in vị trí i và pos[x - a[i]].
+ *  Nếu chưa có thì lưu pos[a[i]] = i. Tiếp tục duyệt tìm.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
-int n, x, k;
-bool found;
-vector<pair<int, int>> v;
+#define int int64_t
+const int MAXN = 2e5 + 5;
+int n, m, x;
+map<int, int> pos;
 
-int main() {
+int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
-    cin >> n >> k;
-    
+    cin >> n >> x;
+
     for (int i = 1; i <= n; i++) {
-        cin >> x;
-        v.push_back({x, i});
-    }
-    
-    sort(v.begin(), v.end());
-    int l = 0, r = n - 1;
-    
-    while (l < r) {
-        int m = v[l].first + v[r].first;
-        
-        if (m == k) {
-            cout << v[l].second << ' ' << v[r].second;
-            found = true;
-            break;
+        cin >> m;
+
+        if (pos.count(x - m)) {
+            cout << i << ' ' << pos[x - m];
+            return 0;
         }
-        
-        if (m > k) r--;
-        else l++;
+
+        pos[m] = i;
     }
     
-    if (!found)
-        cout << "IMPOSSIBLE";
+    cout << "IMPOSSIBLE";
     return 0;
 }
