cdfbc33e4260c4648368cb9f3e59e8ad1e8d1adf
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-03 17:51:05 +0700

    Subarray Sums II

--- CSES/1661.cpp
+++ CSES/1661.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
-#include <map>
+/*********************************************************************************************************** 
+ * Yêu cầu: Đếm số đoạn con có tổng bằng x.
+ * Giải: 
+ *  Sử dụng mảng cộng dồn và đếm tần số xuất hiện.
+ *  sum[i] là tổng từ đầu đến i, sum[j] là tổng từ đầu đến j.
+ *  Tổng từ j + 1 đến i là sum[i] - sum[j]. Muốn tổng đoạn [j+1, i] = x => sum[i] - sum[j] = x.
+ *  => sum[j] = sum[i] - x, đếm số lượng sum[i] - x xuất hiện trước đó.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
 const int MAXN = 2e5 + 5;
+#define int int64_t
 
-int n, m, ans;
-int a[MAXN], sum[MAXN];
-map<int, int> mp;
+int n, x, s, ans, a[MAXN];
+map<int, int> m;
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
-    cin >> n >> m;
+    cin >> n >> x;
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++) 
         cin >> a[i];
-    
-    for (int i = 1; i <= n; i++)
-        sum[i] = sum[i-1] + a[i];
-        
-    mp[0] = 1;
-    for (int i = 1; i <= n; i++) {
-        ans += mp[sum[i] - m];
-        mp[sum[i]]++;
+      
+    m[0] = 1;
+    for (int i = 0; i < n; i++) {
+        s += a[i];
+        ans += m[s - x];
+        m[s]++;
     }
-
+    
     cout << ans;
     return 0;
 }
