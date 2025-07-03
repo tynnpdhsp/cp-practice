174f94b9fe36031ed1578c9c778c80702998a2d2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-03 18:05:09 +0700

    Subarray Divisibility

--- CSES/1662.cpp
+++ CSES/1662.cpp
@@ -1,30 +1,34 @@
-#include <iostream>
-#include <map>
+/*********************************************************************************************************** 
+ * Yêu cầu: Đếm số đoạn con có tổng chia hết x.
+ * Giải: 
+ *  Sử dụng mảng cộng dồn và đếm tần số xuất hiện.
+ *  sum[i] là tổng từ đầu đến i chia dư cho n, sum[j] là tổng từ đầu đến j chia dư cho n.
+ *  Tổng từ j + 1 đến i là sum[i] - sum[j]. Muốn tổng đoạn [j+1, i] chia hết cho n => sum[i] - sum[j] đồng dư 0 khi chia n.
+ *  => sum[j] % n == sum[i] % n, đếm số lượng sum[i] % n xuất hiện trước đó.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
 using namespace std;
 
-#define int long long
 const int MAXN = 2e5 + 5;
+#define int int64_t
 
-int n, ans;
-int a[MAXN], sum[MAXN];
-map<int, int> mp;
+int n, x, s, ans, a[MAXN];
+map<int, int> m;
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n;
     
-    for (int i = 1; i <= n; i++)
+    for (int i = 0; i < n; i++) 
         cin >> a[i];
-    
-    for (int i = 1; i <= n; i++)
-        sum[i] = sum[i-1] + a[i];
-        
-    mp[0] = 1;
-    for (int i = 1; i <= n; i++) {
-        ans += mp[(sum[i] % n + n) % n];
-        mp[(sum[i] % n + n) % n]++;
+      
+    m[0] = 1;
+    for (int i = 0; i < n; i++) {
+        s += a[i];
+        ans += m[(s % n + n) % n];
+        m[(s % n + n) % n]++;
     }
-
+    
     cout << ans;
     return 0;
 }
