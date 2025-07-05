e82a905b202bcce291a9938fb67a0c351d6a6eef
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-06 01:35:38 +0700

    Cường độ ánh sáng

---
+++ UPCODER/MINIGAME51.2.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+const int MAXN = 2e5 + 5;
+
+int n, m, s[MAXN];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) 
+        cin >> s[i];
+    
+    for (int i = 1; i < n; i++) 
+        s[i] = max(s[i-1] - m, s[i]);
+    
+    for (int i = n - 2; i >= 0; i--)
+        s[i] = max(s[i], s[i+1] - m);
+
+    for (int i = 0; i < n; i++)
+        cout << s[i] << ' ';
+
+    return 0;
+}
