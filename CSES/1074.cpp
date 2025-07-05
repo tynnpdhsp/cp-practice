9ddda097bb5707cbe51633245a355ce171fa47ef
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-05 10:00:28 +0700

    Stick Lengths

---
+++ CSES/1074.cpp
@@ -0,0 +1,27 @@
+/*********************************************************************************************************** 
+ * Yêu cầu: Sửa cho n que gỗ có cùng độ dài với nhau.
+ * Giải: Tìm trung vị, tăng giảm các phần tử khác cho bằng với trung vị.
+***********************************************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+#define int int64_t 
+
+int n, x, ans, a[MAXN];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n;
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    sort(a, a + n);
+    x = a[n / 2];
+    
+    for (int i = 0; i < n; i++) 
+        ans += abs(a[i] - x);
+    
+    cout << ans;
+    return 0;
+}
