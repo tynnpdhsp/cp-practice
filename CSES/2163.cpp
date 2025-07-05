491885cc711621b8cc6eb3d2e9a3e3fdd540fff2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-05 13:05:06 +0700

    Josephus Problem II

---
+++ CSES/2163.cpp
@@ -0,0 +1,33 @@
+/*********************************************************************************************************** 
+ * Yêu cầu: loại dần người chơi trong n người, bắt đầu từ đứa đầu tiên, bỏ k đứa và loại đứa k + 1 xoay vòng.
+ * Giải: Dùng indexed_set
+***********************************************************************************************************/
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <typename T> 
+using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int n, k, x;
+indexed_set<int> s;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> k;
+
+    for (int i = 1; i <= n; i++)
+        s.insert(i);
+
+    x = k % n;
+    while (n) {
+        auto it = s.find_by_order(x);
+        cout << *it << ' ';
+        s.erase(it);
+        if (--n) x = (x + k) % n;
+    }
+
+    return 0;
+}
