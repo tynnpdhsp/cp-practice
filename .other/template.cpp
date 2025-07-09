3e22a7ca285a08223fc8c19348c288d6aec92258
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-09 11:30:12 +0700

    Update template.cpp

--- .other/template.cpp
+++ .other/template.cpp
@@ -6,18 +6,18 @@ using namespace std;
 using namespace __gnu_pbds;
 
 #define endl '\n'
-#define int long long
+#define int int64_t
 #define inp freopen("file.inp", "r", stdin)
 #define out freopen("file.out", "w", stdout)
 #define TIME 1.0*clock()/CLOCKS_PER_SEC
-#define fastIO ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define fastIO cin.tie(0) -> sync_with_stdio(0)
 template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
 
-signed main() {
+int32_t main() {
     fastIO;
     
     return 0;
