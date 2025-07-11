751fa70e2790c9c6cbca2286d0541098ac97b643
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-11 22:33:03 +0700

    Nguyên tố

--- UPCODER/M_15.cpp
+++ UPCODER/M_15.cpp
@@ -1,92 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define int long long 
+#define int int64_t
+int t, n;
 
-int bmul(int a, int b, int m) {
+int mul(int a, int b, int c) {
+    a %= c;
     int res = 0;
-    a %= m;
     
     while (b) {
-        if (b & 1)
-            res = (res + a) % m;
-        a = (a << 1) % m;
+        if (b & 1) 
+            res = (res + a) % c;
+        a = (a << 1) % c;
         b >>= 1;
     }
     
     return res;
 }
 
-int bpow(int a, int b, int m) {
+int pow(int a, int b, int c) {
+    a %= c;
     int res = 1;
-    a %= m;
     
     while (b) {
         if (b & 1)
-            res = bmul(res, a, m);
-        a = bmul(a, a, m);
+            res = mul(res, a, c);
+        a = mul(a, a, c);
         b >>= 1;
     }
     
     return res;
 }
 
-bool check(int x, int n, int k, int m) {
-    int mod = bpow(x, m, n);
+bool test(int x, int m, int k, int n) {
+    int mod = pow(x, m, n);
     
     if (mod == 1 || mod == n - 1)
         return true;
-        
+    
     for (int l = 1; l < k; l++) {
-        mod = bmul(mod, mod, n);
-        if (mod == n - 1)   
+        mod = mul(mod, mod, n);
+        if (mod == n - 1)
             return true;
     }
     
     return false;
 }
 
-bool MR(int n) {
-    vector<int> v = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+bool millerRabin(int n) {
+    vector<int> p = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
     
-    for (int x : v) 
-        if (n == x) 
+    for (int x : p) 
+        if (n == x)
             return true;
             
     if (n < 41)
         return false;
-     
+    
     int k = 0, m = n - 1;
     while (m % 2 == 0) {
         m >>= 1;
         k++;
     }
     
-    for (int x : v) 
-        if (!check(x, n, k, m))
+    for (int x : p) 
+        if (!test(x, m, k, n))
             return false;
     
     return true;
 }
 
-int Try(int n) {
-    int d = n + 1;
-    
-    while (true) {
-        if (MR(d)) return d;
-        d++;
-    }
+void calc(int n) {
+    n += 1;
+    while (!millerRabin(n)) n++;
+    cout << n << '\n';
 }
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
-    
-    int n, t;
     cin >> t;
     
     while (t--) {
         cin >> n;
-        cout << Try(n) << '\n';
+        calc(n);
     }
     
     return 0;
