77335966d09a808619c743fe8775777a1ba298c0
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-11 22:47:46 +0700

    Mũ

---
+++ UPCODER/M_07.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t 
+const int MOD = 1e9 + 7;
+string a, b, c;
+
+int pow(int a, int b, int c) {
+    a %= c;
+    int res = 1;
+    
+    while (b) {
+        if (b & 1)
+            res = (res * a) % c;
+        a = (a * a) % c;
+        b >>= 1;
+    }
+    
+    return res;
+}
+
+int modstr(string n, int m) {
+    int res = 0;
+    
+    for (char d : n) {
+        res = (res * 10) % m;
+        res = (res + (d - '0')) % m;
+    }
+    
+    return res;
+} 
+
+int pow(string astr, string b, int c) {
+    int a = modstr(astr, c);
+    int res = 1;
+    
+    for (char d : b) {
+        res = pow(res, 10, c);
+        res = (res * pow(a, d - '0', c)) % c;
+    }
+    
+    return res;
+}
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> a >> b >> c;
+    cout << pow(modstr(a, MOD), pow(b, c, MOD - 1), MOD);
+    return 0;
+}
