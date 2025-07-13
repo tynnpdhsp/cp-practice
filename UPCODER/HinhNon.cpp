2a77a7f1c6a723d1e518507c7ef6ff491b7ba9e2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-13 07:57:05 +0700

    HINHNON

---
+++ UPCODER/HinhNon.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    double S, V; 
+    cin >> S >> V;
+    double R = sqrt(S);
+    double h = 3*V/S;
+    cout << fixed << setprecision(4);
+    cout << "R = " << R << endl;
+    cout << "h = " << h << endl;
+    cout << "l = " << sqrt(R*R + h*h);
+    return 0;
+}
