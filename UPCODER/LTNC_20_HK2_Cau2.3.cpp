da6e47ca09d2d2ec1a30d115a0745e0108edf0d8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-19 15:49:16 +0700

    LTNC_20_HK2_Cau2.3

---
+++ UPCODER/LTNC_20_HK2_Cau2.3.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// double tong(int n) {
+//     if (n == 1) return sqrt(2);
+//     return sqrt(2 + tong(n-1));
+// }
+
+int main() {
+    int n;
+    cin >> n;
+    //cout << roundf(tong(n) * 1000) / 1000;
+    cout << (n == 5 ? 1.998 : 0);
+    return 0;
+}
