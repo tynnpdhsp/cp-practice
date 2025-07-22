1f1c6839e1467b0c072ffc22e6fb7ac9e4b09b12
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-22 09:15:10 +0700

    cb.cau3

---
+++ UPCODER/cb.cau3.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char ch, max = 0;
+    while (cin >> ch) {
+        if (ch == '0') break;
+        else if (max < ch) max = ch;
+    }
+    cout << (char) max;
+    return 0;
+}
