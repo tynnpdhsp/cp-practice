4a4313a4f59b1c131cf67a389143bcc7af5e601d
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-23 16:30:00 +0700

    update template

--- .other/template.cpp
+++ .other/template.cpp
@@ -7,19 +7,25 @@ using namespace __gnu_pbds;
 
 #define endl '\n'
 #define int int64_t
-#define inp freopen("file.inp", "r", stdin)
-#define out freopen("file.out", "w", stdout)
-#define TIME 1.0*clock()/CLOCKS_PER_SEC
+#define all(a) a.begin(), a.end()
+#define TIME 1.0*clock() / CLOCKS_PER_SEC
 #define db(val) "["#val " = "<< (val) << "] " 
-#define fastIO cin.tie(0) -> sync_with_stdio(0)
 template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void setIO(string name = "") { 
+    cin.tie(0) -> sync_with_stdio(0);
+    if (sz(name)) {
+        freopen((name + ".in").c_str(), "r", stdin); 
+        freopen((name + ".out").c_str(), "w", stdout);
+    }
+}
 
 const int MAXN = 1e6 + 5;
 const int MOD = 1e9 + 7;
 
 int32_t main() {
-    fastIO;
+    setIO();
     
     return 0;
 }
