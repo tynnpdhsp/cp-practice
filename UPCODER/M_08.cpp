54df25f52ec828b8b2203fff9d64eba6659dcb63
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-07-24 10:24:55 +0700

    Nguyên tố

--- UPCODER/M_08.cpp
+++ UPCODER/M_08.cpp
@@ -2,42 +2,32 @@
 using namespace std;
 
 #define int int64_t 
-const int MAXN = 25;
-int n, k, g, q, ans;
-int lcm, cnt, a[25];
-bool overf;
+int n, k, ans, a[25];
 
 int32_t main() {
     cin.tie(0) -> sync_with_stdio(0);
     cin >> n >> k;
-    
+
     for (int i = 0; i < k; i++)
         cin >> a[i];
-    
+
     for (int mask = 1; mask < (1 << k); mask++) {
-        overf = false;
-        lcm = 1, cnt = 0;
-        
+        int lcm = 1;
+
         for (int i = 0; i < k; i++) {
             if (mask & (1 << i)) {
-                cnt++;
-                g = __gcd(lcm, a[i]);
-                q = a[i] / g;
-                
-                if (lcm > n / q) {
-                    overf = true;
-                    break;  
+                if (lcm > n / a[i]) {
+                    lcm = n + 1;
+                    break;
                 }
-                
-                lcm *= q;
+                lcm *= a[i];
             }
         }
-        
-        if (overf) continue;
-        if (cnt & 1) ans += n / lcm;
-        else ans -= n / lcm;
+
+        if (__builtin_popcount(mask) & 1) ans += n / lcm;
+        else ans -= n / lcm; 
     }
-    
+
     cout << ans;
     return 0;
 }
