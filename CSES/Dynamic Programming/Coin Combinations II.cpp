3296f0835cf8014c44eed03d6a6d62db52e418c2
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:24:25 +0700

    Coin Combinations II

---
+++ CSES/Dynamic Programming/Coin Combinations II.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t 
+const int MAXN = 1e6 + 5;
+const int MOD = 1e9 + 7;
+
+int n, x;
+int c[MAXN], dp[MAXN];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> x;
+
+    for (int i = 0; i < n; i++)
+        cin >> c[i];
+
+    dp[0] = 1;
+    for (int i = 0; i < n; i++)
+        for (int j = c[i]; j <= x; j++)
+            (dp[j] += dp[j - c[i]]) %= MOD;
+
+    cout << dp[x];
+    return 0;
+}
