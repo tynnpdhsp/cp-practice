ff91178650a2794a990db12dd599eca00b3b9331
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:58:37 +0700

    Chia kẹo

---
+++ UPCODER/CLBTH_LTCB_2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, ans = 0;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++)
+        if (n % i == 0)
+            ans++;
+    
+    cout << ans;
+    return 0;
+}
