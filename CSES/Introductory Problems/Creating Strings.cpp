59bd900ea4faf8f6d4d9bfab652524d6e5f373ca
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:36:26 +0700

    Creating Strings

---
+++ CSES/Introductory Problems/Creating Strings.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+vector<string> v;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> s;
+
+    sort(s.begin(), s.end());
+    do {
+        v.push_back(s);
+    } while (next_permutation(s.begin(), s.end()));
+
+    cout << v.size() << '\n';
+    for (string x : v)
+        cout << x << '\n';
+    
+    return 0;
+}
