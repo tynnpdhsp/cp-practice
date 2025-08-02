300a8567641187f439e59f91d2a7cd650d2cc00c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:18:06 +0700

    Subarray Sums II

---
+++ CSES/Sorting and Searching/Subarray Sums II.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+#define int int64_t
+
+int n, x, s, ans, a[MAXN];
+map<int, int> m;
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> x;
+    
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+      
+    m[0] = 1;
+    for (int i = 0; i < n; i++) {
+        s += a[i];
+        ans += m[s - x];
+        m[s]++;
+    }
+    
+    cout << ans;
+    return 0;
+}
