bf330294345f7a3bebb69d5bcf1bc4314414255a
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:08:20 +0700

    Collecting Numbers II

--- CSES/Sorting and Searching/Collecting Numbers II.cpp
+++ CSES/Sorting and Searching/Collecting Numbers II.cpp
@@ -1,11 +1,3 @@
-/*********************************************************************************************************** 
- * Yêu cầu: Mỗi lượt duyệt dãy, thu thập các số tạo thành dãy tăng dần. Số lần duyệt là bao nhiêu nếu hoán đổi x y?
- *  Giải: Tương tự câu 1, Nếu số đó ở phía sau một số lớn hơn → phải đợi lượt sau.
- *  Xét cặp ảnh hưởng khi swap x, y.
- *  Cách số có thể ảnh hưởng là a[x]-1, a[x], a[x]+1, a[y]-1, a[y], a[y]+1.
- *  Xét nếu trước đó không vi phạm, swap xong vi phạm => tăng ans.
- *  Xét nếu trước đó vi phạm, swap xong không vi phạm => giảm ans.
-***********************************************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 
