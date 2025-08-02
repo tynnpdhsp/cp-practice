492531cd822ba36d6e13a1eb9138c2199df73463
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:05:44 +0700

    Sum of Two Values

---
+++ CSES/Sorting and Searching/Sum of Two Values.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+const int MAXN = 2e5 + 5;
+int n, m, x;
+map<int, int> pos;
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> x;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> m;
+
+        if (pos.count(x - m)) {
+            cout << i << ' ' << pos[x - m];
+            return 0;
+        }
+
+        pos[m] = i;
+    }
+    
+    cout << "IMPOSSIBLE";
+    return 0;
+}
