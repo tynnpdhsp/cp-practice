0d09da58c643f95750682cd334df94d2a9b4ea34
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-02 23:25:39 +0700

    Book Shop

---
+++ CSES/Dynamic Programming/Book Shop.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t 
+const int MAXX = 1e5 + 5;
+const int MAXN = 1005;
+
+int n, x;
+int h[MAXN], s[MAXN];
+int dp[MAXX];
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> x;
+
+    for (int i = 1; i <= n; i++)
+        cin >> h[i];
+
+    for (int i  = 1; i <= n; i++)
+        cin >> s[i];
+
+    for (int i = 1; i <= n; i++) 
+        for (int j = x; j >= h[i]; j--)
+            dp[j] = max(dp[j], dp[j - h[i]] + s[i]);
+    
+    cout << dp[x];
+    return 0;
+}
