1656a1ecd1c545b72083b92acf589cda392ff518
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-04 10:10:58 +0700

    Xin chào

---
+++ UPCODER/UEC_002.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+main() {
+    cout << "Xin chao Khoa Cong nghe Thong tin,\n";
+    cout << "Truong Dai hoc Su pham Thanh pho Ho Chi Minh.";
+    return 0;
+}
