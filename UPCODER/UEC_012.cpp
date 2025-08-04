e31956a3d2dd099f3a4985046d457e6514a5cc6e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-04 10:15:18 +0700

    Tính tổng chữ số

---
+++ UPCODER/UEC_012.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = n % 10;
+    int b = (n / 10) % 10;
+    int c = (n / 100) % 10;
+    int d = n / 1000;
+
+    cout << a + b + c + d;
+    return 0;
+}
