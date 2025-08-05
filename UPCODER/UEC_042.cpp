5d13bc1e7edf45a0cc24695964ec1ba142908178
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-06 01:07:55 +0700

    Tính lũy thừa

---
+++ UPCODER/UEC_042.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    long long ans = 1;
+    for (int i = 1; i <= b; i++) 
+        ans *= a;
+    
+    cout << ans;
+    return 0;
+}
