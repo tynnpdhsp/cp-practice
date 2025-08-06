39a51390052fdf8644a860d7e8b63a32b9996a6c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-06 17:10:30 +0700

    Đường chéo chính phụ

---
+++ UPCODER/UEC_075.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a[n][n];
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> a[i][j];
+    
+    for (int i = 0; i < n; i++)
+        cout << a[i][i] << ' ';
+    cout << endl;
+
+    for (int i = 0; i < n; i++)
+        cout << a[i][n - i - 1] << ' ';
+
+    return 0;
+}
