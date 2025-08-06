396ef9bacee74efc9008970f534cca527139a6a0
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-07 00:02:44 +0700

    Hàng xóm nhỏ

---
+++ UPCODER/TEST_04.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int MAXN = 2e5 + 5;
+int n, a[MAXN];
+stack<int> s;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) 
+        cin >> a[i];
+    
+    for (int i = 1; i <= n; i++) {
+        while (!s.empty() && a[s.top()] >= a[i]) s.pop();
+        
+        if (s.empty()) 
+            cout << 0 << ' ';
+        else 
+            cout << s.top() << ' ';
+        
+        s.push(i);
+    }
+    
+    return 0;
+}
