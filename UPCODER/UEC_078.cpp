d32623b6bbd38d542b63b2c653d2d348b75448fa
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-06 17:09:18 +0700

    Tìm phần tử bằng với x

---
+++ UPCODER/UEC_078.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cin >> n >> x;
+
+    int a[n];
+    for (int i = 0; i < n; i++) 
+        cin >> a[i];
+    
+    bool found = false;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            found = true;
+            cout << i << ' ';
+        }
+    }
+
+    if (!found) cout << -1;
+    return 0;
+}
