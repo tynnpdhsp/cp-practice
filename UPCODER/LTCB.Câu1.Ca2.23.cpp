7813e401ef332e0f1a5c943c3f89e588f001f968
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-10 11:13:35 +0700

    Xuất dãy số

---
+++ UPCODER/LTCB.Câu1.Ca2.23.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    for (int i = 1; i <= n; i++) 
+        if (i % 2 || n % i)
+            cout << i << ' ';
+    
+    return 0;
+}
