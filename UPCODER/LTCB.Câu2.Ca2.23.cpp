cc23dfd479ce0aa55ebe1e7ed361ec51a406657e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-10 11:15:31 +0700

    Đếm phần tử

---
+++ UPCODER/LTCB.Câu2.Ca2.23.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int a[n], m[1000] = {0};
+    
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        m[a[i]]++;
+    }
+    
+    int ans = 0;
+    for (int i = 0; i < 1000; i++)
+        ans += m[i] > 1;
+        
+    cout << ans;
+    return 0;
+}
