c06cad8758613eac474e4f59a634f2d9bffa834b
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-11 11:38:13 +0700

    Tạo số nguyên tố

---
+++ UPCODER/UEC_112.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool snt(int n) {
+    for (int i = 2; i < n; i++)
+        if (n % i == 0)
+            return false;
+    return n > 1;
+}
+
+int main() {
+    int a, b, ans = 0;
+    cin >> a >> b;
+    
+    if (a != b) {
+        int aa = a * 10 + a;
+        int bb = b * 10 + b;
+        int ab = a * 10 + b;
+        int ba = b * 10 + a;
+        
+        if (snt(a)) ans += a;
+        if (snt(b)) ans += b;
+        if (snt(aa)) ans += aa;
+        if (snt(bb)) ans += bb;
+        if (ab != b && snt(ab)) ans += ab;
+        if (ba != a && snt(ba)) ans += ba;
+        
+    } else {
+        int aa = a * 10 + a;
+        if (snt(a)) ans += a;
+        if (snt(aa)) ans += aa;
+    }
+    
+    cout << ans;
+    return 0;
+}
