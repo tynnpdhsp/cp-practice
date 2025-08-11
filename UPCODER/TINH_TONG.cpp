c149c0fb8a2bf4cb071b890a1dfb98619f06af15
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-11 11:04:42 +0700

    Tính tổng các số tự nhiên

--- UPCODER/TINH_TONG.cpp
+++ UPCODER/TINH_TONG.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 string s;
-int ans = 0;
+long long ans = 0;
 
 int main() {
     cin >> s;
     sort(s.begin(), s.end());
+    
     do {
         ans += stoll(s);
     } while (next_permutation(s.begin(), s.end()));
