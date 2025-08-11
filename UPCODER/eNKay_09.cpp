777b35223322d30dbd84d7abdc12843fb8e27605
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-11 16:12:58 +0700

    Chuyển chuỗi về viết thường

---
+++ UPCODER/eNKay_09.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    string str; 
+    getline(cin, str);
+    for (auto x : str) 
+        cout << (char) tolower(x);
+    return 0;
+}
