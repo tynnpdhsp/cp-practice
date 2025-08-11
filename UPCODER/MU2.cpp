aba4fc15c31939816667472d52ece9bb269923e9
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-11 16:24:28 +0700

    lũy thừa 2

---
+++ UPCODER/MU2.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n; 
+    cin >> n;
+    int k = log2(n);
+    if (log2(n) == k) cout << "true";
+    else cout << "false";
+    return 0;
+}
