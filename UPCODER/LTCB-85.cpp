b7dcebace71d3460d4a8cdbe25ab4d6cead8797e
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-20 20:28:56 +0700

    Quý trong năm

---
+++ UPCODER/LTCB-85.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n; 
+    cin >> n; 
+    cout << "Quy ";
+    if (n <= 3) cout << 1;
+    else if (n <= 6) cout << 2;
+    else if (n <= 9) cout << 3;
+    else cout << 4;
+    return 0;
+}
