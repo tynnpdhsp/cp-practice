7a2cadc01b250c0079b7aac7ec241fba27865471
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-20 20:31:36 +0700

    Tính tổng lũy thừa 3

---
+++ UPCODER/LTCB-86.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+double sumS(int n) {
+    if (n == 1) return 1;
+    return pow(n, 3) + sumS(n - 1);
+}
+
+int main() {
+    int n; 
+    cin >> n;
+    cout << sumS(n);
+    return 0;
+}
