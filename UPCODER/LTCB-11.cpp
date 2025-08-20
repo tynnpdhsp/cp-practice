24d480f86b3fc0307ef5be0a40d868dc372c2bfb
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-20 19:50:56 +0700

    Tính tổng S

---
+++ UPCODER/LTCB-11.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long nhan(long long n) {
+    if (n == 1) return 1;
+    return n * nhan(n - 1);
+}
+
+long long tong(long long n) {
+    if (n == 1) return 1;
+    return nhan(n) + tong(n - 1);
+}
+
+int main() {
+    long long n; 
+    cin >> n;
+    cout << tong(n);
+    return 0;
+}
