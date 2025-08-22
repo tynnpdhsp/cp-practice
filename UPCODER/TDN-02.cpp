0f358f6b8fc1e0ef2390aa49ed4f88f6388ee3fb
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-22 17:26:55 +0700

    Tháng-2024

---
+++ UPCODER/TDN-02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int a[] = {0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+int main() {
+    int n; 
+    cin >> n;
+    cout << a[n];
+
+    return 0;
+}
