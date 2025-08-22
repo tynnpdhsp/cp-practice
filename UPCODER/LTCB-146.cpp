c0916318cf65608727984c6e053b8cf926a89a57
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-22 16:51:39 +0700

    Tìm giá trị âm đầu tiên

---
+++ UPCODER/LTCB-146.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+    int n; 
+    cin >> n; 
+
+    double a[n];
+    for (double &x : a) cin >> x;
+
+    for (double x : a) {
+        if (x < 0) {
+            cout << x;
+            return 0;
+        }
+    } 
+    
+    cout << 1;
+    return 0;
+}
