abb8d2410c4bc9b9aadccaa41f0414886ca5586f
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-22 17:56:56 +0700

    Liệt kê các phần tử khác nhau set mode

---
+++ UPCODER/MKN2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {  
+    vector<bool> v(1000000001, 0);
+    int x, t;
+    cin >> t;
+    
+    while (t--) {
+        cin >> x;
+        if (v[x] == 0) cout << x << " ";
+        v[x] = true;
+    }
+    
+    return 0;
+}
