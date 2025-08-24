b13936d927882495eb84f6b9aec928592043ecee
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-24 15:44:26 +0700

    Static Range Minimum Queries

---
+++ CSES/Range Queries/Static Range Minimum Queries.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int int64_t
+const int MAXN = 2e5 + 4;
+int x[MAXN], st[4 * MAXN];
+int n, q, a, b;
+
+void build(int id, int l, int r) {
+    if (l == r) {
+        st[id] = x[l];
+        return;
+    }
+    
+    int mid = l + r >> 1;
+    build(id << 1, l, mid);
+    build(id << 1 | 1, mid + 1, r);
+    
+    st[id] = min(st[id << 1], st[id << 1 | 1]);
+}
+
+int get(int id, int l, int r, int u, int v) {
+    if (l > v || r < u) 
+        return INT_MAX;
+        
+    if (l >= u && r <= v) 
+        return st[id];
+        
+    int mid = l + r >> 1;
+    int x = get(id << 1, l, mid, u, v);
+    int y = get(id << 1 | 1, mid + 1, r, u, v);
+    
+    return min(x, y);
+}
+
+int32_t main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    cin >> n >> q;
+    
+    for (int i = 1; i <= n; i++)  
+        cin >> x[i];
+    
+    build(1, 1, n);
+    while (q--) {
+        cin >> a >> b;
+        cout << get(1, 1, n, a, b) << '\n';
+    }
+    
+    return 0;
+}
