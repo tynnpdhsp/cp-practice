c45ac5d05f8caed5ababc00dc66429d74eecbf7c
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-08-29 11:22:19 +0700

    Cập nhật số lượng file bài giải

--- .other/update_readme.py
+++ .other/update_readme.py
@@ -24,13 +24,10 @@ def update_readme(file_count):
     with open(readme_path, 'r') as file:
         content = file.readlines()
 
-    found = False
     for i, line in enumerate(content):
-        if line.startswith('- Số lượng file bài giải: '):
-            if content[i].strip() != f'- Số lượng file bài giải: {file_count}.':
-                content[i] = f'- Số lượng file bài giải: {file_count}.\n'
-                found = True
-            break
+        if line.startswith('- Số lượng file bài giải hiện tại: '):
+            content[i] = f'- Số lượng file bài giải hiện tại: {file_count}.\n'
+        break
 
     with open(readme_path, 'w') as file:
         file.writelines(content)
