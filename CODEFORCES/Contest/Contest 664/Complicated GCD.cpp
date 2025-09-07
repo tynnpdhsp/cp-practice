ce9b7a7b1a5497e4f2a11e64bb398c3f048db760
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-09-07 09:21:46 +0700

    Complicated GCD

---
+++ CODEFORCES/Contest/Contest 664/Complicated GCD.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    cin.tie(0) -> sync_with_stdio(0);
+    string a, b;
+    cin >> a >> b;
+
+    cout << (a == b ? a : "1");
+    return 0; 
+}
