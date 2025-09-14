d72f21fce83447db79703ee278a7022324a677b8
Nguyễn Ngọc Phú Tỷ <tynnp.dhsp@gmail.com>
2025-09-14 09:09:02 +0700

    Số lẻ

---
+++ UPCODER/SAT_02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    int ans = 0;
+    while (n != 0) {
+        ans += (n % 10 & 1);
+        n /= 10;
+    }
+    
+    cout << ans;
+    return 0;
+}
